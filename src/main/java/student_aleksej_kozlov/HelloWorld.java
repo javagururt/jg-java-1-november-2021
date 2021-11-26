515a813d65a51d9c445a48898076548445112705
Aleksej_K <axel.k@inbox.lv>
2021-11-26 16:00:55 +0200

    Hello World

--- src/main/java/student_aleksej_kozlov/HelloWorld.java
+++ src/main/java/student_aleksej_kozlov/HelloWorld.java
@@ -3,6 +3,6 @@ package student_aleksej_kozlov;
 public class HelloWorld
 {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World 1");
     }
 }
