bf13b5afcca45aaecd7f6b52083d699d6ad27b50
ValeriaKul <lera.dribnyuk@mail.ru>
2021-11-30 15:46:59 +0200

    ;)

--- src/main/java/student_valeriia_kulikova/Lesson2_Ex9.java
+++ src/main/java/student_valeriia_kulikova/Lesson2_Ex9.java
@@ -5,8 +5,17 @@ import java.util.Scanner;
 public class Lesson2_Ex9 {
     public static void main(String [] args) {
 
-        Scanner scanner = new Scanner(System.in)
+        Scanner scanner = new Scanner(System.in);
             System.out.println("write NUMBER");
-         
+        int Num1 = scanner.nextInt();
+
+        for(int i = 1; i < 11; i++) {
+
+            int Mult=Num1 * i;
+            System.out.println(Num1 + " x " + i + " = " + Mult);
+        }
+
     }
+         
+
 }
