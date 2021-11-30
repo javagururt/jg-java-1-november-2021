01e4a84ecde191a4888075bfcee3ad5af2ed1d0a
mihailsnikolajevs <mike2309@inbox.lv>
2021-11-30 18:22:59 +0000

    Hello world
    Signed-off-by: mihailsnikolajevs <mike2309@inbox.lv>

--- src/main/java/student_mihails_nikolajevs/HelloWorld.java
+++ src/main/java/student_mihails_nikolajevs/HelloWorld.java
@@ -1,6 +1,10 @@
 package student_mihails_nikolajevs;
 
-public class HelloWorld
-{
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
 }
+
 
