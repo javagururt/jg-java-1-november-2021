dfd276ba4e81f2a05e67d34d2fd60d8d2ff9742a
ValeriaKul <lera.dribnyuk@mail.ru>
2021-11-30 04:04:12 +0200

    ;)

--- src/main/java/student_valeriia_kulikova/Lesson2_Ex1.java
+++ src/main/java/student_valeriia_kulikova/Lesson2_Ex1.java
@@ -3,21 +3,24 @@ package student_valeriia_kulikova;
 import java.util.Scanner;
 
 public class Lesson2_Ex1 {
-    public static void main(String args []) {
+    public static void main(String [] args) {
 
         Scanner scanner = new Scanner(System.in);
+        System.out.println("write the FIRST number and press ENTER");
         int firstNumber = scanner.nextInt();
+
+        System.out.println("write the SECOND number and press ENTER");
         int secondNumber = scanner.nextInt();
-        int Сумма = firstNumber + secondNumber;
-        int Вычитание = firstNumber - secondNumber;
-        int Умножение = firstNumber * secondNumber;
-        int Деление = firstNumber / secondNumber;
+        int Сумма, Вычитание, Умножение, Деление;
+
+        Сумма = firstNumber + secondNumber;
+        Вычитание = firstNumber - secondNumber;
+        Умножение = firstNumber * secondNumber;
+        Деление = firstNumber / secondNumber;
 
         System.out.println("Сумма = " + Сумма);
         System.out.println("Вычитание = " + Вычитание);
         System.out.println("Умножение = " + Умножение);
         System.out.println("Деление = " + Деление);
-
-
     }
 }
