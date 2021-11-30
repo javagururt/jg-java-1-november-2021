720de92847b1e208b6489f261ee36d96b9019051
StanislavP <Perminovsb@mail.ee>
2021-11-30 14:39:54 +0200

    Lesson1 and lesson2 (task1-10)

---
+++ src/main/java/student_stanislav_p/Lesson2_Ex4.java
@@ -0,0 +1,11 @@
+package student_stanislav_p;
+
+public class Lesson2_Ex4 {
+    static class Phone {
+
+        public static void main(String[] args) {
+            
+        }
+    }
+
+}
---
+++ src/main/java/student_stanislav_p/Lesson2_Ex5.java
@@ -0,0 +1,11 @@
+package student_stanislav_p;
+
+public class Lesson2_Ex5 {
+    static class BankAccount {
+
+        public static void main(String[] args) {
+            
+        }
+    }
+
+}
