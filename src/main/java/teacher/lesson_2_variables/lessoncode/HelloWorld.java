bc8dd4c82cb73b19f3986e5e181fc7f679b403d4
Ashvegzhda <ashvegzhda@icloud.com>
2021-12-02 21:57:51 +0200

    All your bases are now belong to us

--- src/main/java/teacher/lesson_2_variables/lessoncode/HelloWorld.java
+++ src/main/java/teacher/lesson_2_variables/lessoncode/HelloWorld.java
@@ -1,9 +1,41 @@
 package teacher.lesson_2_variables.lessoncode;
 
-public class HelloWorld {
+public class Armstrong {
 
     public static void main(String[] args) {
-        System.out.println(123);
-        System.out.println("Hello World!");
+
+        int low = 999, high = 99999;
+
+        for(int number = low + 1; number < high; ++number) {
+
+            if (checkArmstrong(number))
+                System.out.print(number + " ");
+        }
+    }
+
+    public static boolean checkArmstrong(int num) {
+        int digits = 0;
+        int result = 0;
+        int originalNumber = num;
+
+        // number of digits calculation
+        while (originalNumber != 0) {
+            originalNumber /= 10;
+            ++digits;
+        }
+
+        originalNumber = num;
+
+        // result contains sum of nth power of its digits
+        while (originalNumber != 0) {
+            int remainder = originalNumber % 10;
+            result += Math.pow(remainder, digits);
+            originalNumber /= 10;
+        }
+
+        if (result == num)
+            return true;
+
+        return false;
     }
 }
