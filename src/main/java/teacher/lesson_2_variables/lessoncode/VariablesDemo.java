d7ea0eecbe083680472320b2a4a00a9f04b27b90
Ruslans Tufetulovs <ruslans.tufetulovs@gmail.com>
2021-12-02 21:52:42 +0200

    lesson 2 variables

---
+++ src/main/java/teacher/lesson_2_variables/lessoncode/VariablesDemo.java
@@ -0,0 +1,25 @@
+package teacher.lesson_2_variables.lessoncode;
+
+class VariablesDemo {
+
+    public static void main(String[] args) {
+        int age = 10;
+        int x = 20;
+        int y = 30;
+
+        System.out.println(age);
+
+        int sumResult = x + y;
+        System.out.println("Sum result: " + sumResult); //sum
+
+        System.out.println("Sum result = " + (x + y)); //sum
+        System.out.println("Sum result = " + x + y); //string concat
+
+        double bankBalance = 100000.00;
+        System.out.println(bankBalance);
+
+        boolean thirsty = true;
+        System.out.println(thirsty);
+    }
+
+}
