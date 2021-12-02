95381b8a82d3f2b04b76b172c91855a594d60ac3
Ruslans Tufetulovs <ruslans.tufetulovs@gmail.com>
2021-12-02 21:23:26 +0200

    lesson 2 hello world

---
+++ src/main/java/teacher/lesson_2_variables/lessoncode/HelloWorld.java
@@ -0,0 +1,9 @@
+package teacher.lesson_2_variables.lessoncode;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println(123);
+        System.out.println("Hello World!");
+    }
+}
