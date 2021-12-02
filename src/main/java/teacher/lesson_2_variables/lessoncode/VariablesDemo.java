c5d57646994f56c51465f6edb85eb2f8a9dfb8f5
StanislavP <Perminovsb@mail.ee>
2021-12-02 21:59:33 +0200

    Lesson1 and lesson2

--- src/main/java/teacher/lesson_2_variables/lessoncode/VariablesDemo.java
+++ src/main/java/teacher/lesson_2_variables/lessoncode/VariablesDemo.java
@@ -3,17 +3,17 @@ package teacher.lesson_2_variables.lessoncode;
 class       VariablesDemo {
 
     public static void main(String[] args) {
-        int age = 10;
+        int age = 40;
         int x = 20;
-        int y = 30;
+        int h = 30;
 
         System.out.println(age);
 
-        int sumResult = x + y;
+        int sumResult = x + h;
         System.out.println("Sum result: " + sumResult); //sum
 
-        System.out.println("Sum result = " + (x + y)); //sum
-        System.out.println("Sum result = " + x + y); //string concat
+        System.out.println("Sum result = " + (x + h)); //sum
+        System.out.println("Sum result = " + x + h); //string concat
 
         double bankBalance = 100000.00;
         System.out.println(bankBalance);
