ea5099856895ff2518de5f78695ce5967be84ef3
DmitrijT <di_riga@inbox.lv>
2021-12-03 23:47:32 +0200

    :)

--- src/main/java/student_dima_talanov/lesson_2/level_3_junior/Task_11.java
+++ src/main/java/student_dima_talanov/lesson_2/level_3_junior/Task_11.java
@@ -5,10 +5,13 @@ public class Task_11 {
 
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        double first = scanner.nextFloat();
-        double second = scanner.nextFloat();
-        double third = scanner.nextFloat();
-        double result = (first + second + third) / 3;
+        System.out.print("Enter first number: ");
+        int first = scanner.nextInt();
+        System.out.print("Enter second number: ");
+        int second = scanner.nextInt();
+        System.out.print("Enter third number: ");
+        int third = scanner.nextInt();
+        float result = (first + second + third)  / 3;
         System.out.println("Result is = " + result);
     }
 }
