16d0d06a9b6bf623e09f5af4c7c43d9b8465dc63
DmitrijT <di_riga@inbox.lv>
2021-12-04 17:15:42 +0200

    :)

---
+++ src/main/java/student_dima_talanov/level_6_middle/AttentionPlease.java
@@ -0,0 +1,9 @@
+package student_dima_talanov.level_6_middle;
+
+public class AttentionPlease {
+
+    public static void main(String[] args) {
+    System.out.println("Code outside the method will not compile!");
+    }
+}
+
