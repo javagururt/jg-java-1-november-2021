f246393e59ef7d1136ca7ce4e01a4599ab3e099a
Andrei Karamnov <unrealandre@gmail.com>
2021-12-04 22:04:39 +0200

    homework level 1,2,3

---
+++ src/main/java/student_andrei_karamnov/lesson_2_variables/level_2_intern/BankAccount.java
@@ -0,0 +1,15 @@
+package student_andrei_karamnov.lesson_2_variables.level_2_intern;
+
+
+/*Дан код, нужно исправить ошибку компиляции.
+Для сдачи работы создайте исправленный класс.
+
+class BankAccount {
+
+	public static void main(String[] args) {
+
+}*/
+public class BankAccount {
+    public static void main(String[] args) {
+    }
+}
