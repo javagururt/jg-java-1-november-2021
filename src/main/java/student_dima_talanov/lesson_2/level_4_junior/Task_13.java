e08b35dfaefc2ac8bb8bd26a37b7d40c93ece47b
DmitrijT <di_riga@inbox.lv>
2021-12-04 15:24:51 +0200

    :)

---
+++ src/main/java/student_dima_talanov/lesson_2/level_4_junior/Task_13.java
@@ -0,0 +1,4 @@
+package student_dima_talanov.lesson_2.level_4_junior;
+
+public class Task_13 {
+}
---
+++ src/main/java/student_dima_talanov/level_5_middle/Task_15.java
@@ -0,0 +1,9 @@
+package student_dima_talanov.level_5_middle;
+
+
+public class Task_15 {
+
+    public static void main(String[] args) {
+        System.out.print("text");
+    }
+}
