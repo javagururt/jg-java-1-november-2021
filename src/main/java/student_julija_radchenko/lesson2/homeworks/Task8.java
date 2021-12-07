bf1dc022fb9c41e0080a525c50850747b679b24e
JulijaRadchenko <julija_radchenko@inbox.lv>
2021-12-08 00:06:30 +0200

    HomeWork_Lesson2_task8

---
+++ src/main/java/student_julija_radchenko/lesson2/homeworks/Task8.java
@@ -0,0 +1,14 @@
+package student_julija_radchenko.lesson2.homeworks;
+
+import java.util.Scanner;
+
+public class Task8 {
+
+public static void main(String[] args){
+    System.out.println("What't is your name?");
+    Scanner enterYourName = new Scanner(System.in);
+    String YourName = enterYourName.nextLine();
+    System.out.println("Hallo" + enterYourName);
+
+}
+}
