ec7ab2f367965cc2f522a1aad093c1c5949e8d87
RolandLV007 <darbameils@inbox.lv>
2021-12-09 14:42:12 +0200

    Merge remote-tracking branch 'origin/master'

--- src/main/java/student_rolands_gudels/homework/lesson2_variables/level7/Task_29.java
+++ src/main/java/student_rolands_gudels/homework/lesson2_variables/level7/Task_29.java
@@ -2,30 +2,25 @@ package student_rolands_gudels.homework.lesson2_variables.level7;
 
 public class Task_29 {
 
-    //Исправьте ошибки компиляции в данной программе.
-    //
-    //public class SumTwoNumbers {
-    //
-    //	public static void main(String[] args) {
-    //
-    //		int numberOne;
-    //		int numberTwo;
-    //
-    //		System.out.println("Number 1 = " + numberOne);
-    //		System.out.println("Number 2 = " + numberTwo);
-    //
-    //		int numberOne = 10;
-    //		int numberTwo = 20;
-    //
-    //		int sum = numberOne + numberTwo;
-    //
-    //		System.out.println("Sum = " + sum);
-    //	}
-    //
-    //}
-    //
-    //Подсказка: в Java использовать переменные нельзя до тех пор пока
-    //им не будет присвоено начальное значение.
-}
+
+    	public static void main(String[] args) {
+
+    		int numberOne = 10;
+            int numberTwo = 20;
+
+    		System.out.println("Number 1 = " + numberOne);
+    		System.out.println("Number 2 = " + numberTwo);
+
+
+
+    		int sum = numberOne + numberTwo;
+
+    		System.out.println("Sum = " + sum);
+    	}
+
+    }
+
+
+
 
 
