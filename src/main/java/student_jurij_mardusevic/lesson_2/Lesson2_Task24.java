f476045b2dc1587601cd8a4745bf4417ce13571f
Jurij <jurijmardusevic@gmail.com>
2021-12-09 19:26:01 +0200

    Task29

--- src/main/java/student_jurij_mardusevic/lesson_2/Lesson2_Task24.java
+++ src/main/java/student_jurij_mardusevic/lesson_2/Lesson2_Task24.java
@@ -1,6 +1,6 @@
 package student_jurij_mardusevic.lesson_2;
 
-public class Lesson24_Task24 {
+public class Lesson2_Task24 {
     public static void main(String[] args) {
         System.out.println("I wanna coffee");
         System.out.println("So bad");
