3d6c64f281f179f8ad68fa3d20da167367f1c3e5
DmitrijT <di_riga@inbox.lv>
2021-12-12 01:14:45 +0200

    :)

---
+++ src/main/java/student_dima_talanov/lesson_3/level_1_intern/Task_1_2_3_4_5_6/Robot.java
@@ -0,0 +1,11 @@
+package student_dima_talanov.lesson_3.level_1_intern.Task_1_2_3_4_5_6;
+
+public class Robot {
+
+    public void sayHello() { System.out.println("Hello!"); }
+    public void sayYourName() {System.out.println("My name is Robo"); }
+
+
+    public Robot() {
+    }
+}
