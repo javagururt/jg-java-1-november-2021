b10d2ce4b47277254842721765152c4f235c3bb3
StanislavP <Perminovsb@mail.ee>
2021-12-12 03:42:17 +0200

    Lesson1,  lesson2, lesson3

--- src/main/java/student_stanislav_p/lesson_3_oop_first_look/homework/level_1_intern/Robot.java
+++ src/main/java/student_stanislav_p/lesson_3_oop_first_look/homework/level_1_intern/Robot.java
@@ -13,11 +13,12 @@ public class Robot {
     }
 
     public Robot(String robotName) {
+
         this.name1 = robotName;
     }
 
     public static void main(String[] args) {
-        student_stanislav_p.lesson_3_oop_first_look.homework.level_1_intern.Robot robot = new student_stanislav_p.lesson_3_oop_first_look.homework.level_1_intern.Robot("John");
+        student_stanislav_p.lesson_3_oop_first_look.homework.level_1_intern.Robot robot = new student_stanislav_p.lesson_3_oop_first_look.homework.level_1_intern.Robot("ROBO");
         robot.sayHello();
         robot.sayYourName();
     }
