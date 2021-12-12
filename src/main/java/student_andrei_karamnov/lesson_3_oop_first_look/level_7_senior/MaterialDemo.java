c66f3fc7f33e9407d69a3c2f293f45da039a22be
Andrei Karamnov <unrealandre@gmail.com>
2021-12-12 22:58:24 +0200

    homework lesson 3

---
+++ src/main/java/student_andrei_karamnov/lesson_3_oop_first_look/level_7_senior/MaterialDemo.java
@@ -0,0 +1,8 @@
+package student_andrei_karamnov.lesson_3_oop_first_look.level_7_senior;
+
+class MaterialDemo {
+    public static void main(String[] args) {
+        Material material = new Material();
+        material.Ferum("Ferix-3\n","IronRussia");
+    }
+}
