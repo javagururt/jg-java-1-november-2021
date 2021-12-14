232718389cde91fe1df1bed0285ec13b1ec58ff4
RolandLV007 <darbameils@inbox.lv>
2021-12-14 18:05:13 +0200

    Merge remote-tracking branch 'origin/master'

---
+++ src/main/java/student_rolands_gudels/lesson_3_oop_first_look/level_7_senior/ComputerDemo.java
@@ -0,0 +1,12 @@
+package student_rolands_gudels.lesson_3_oop_first_look.level_7_senior;
+
+ class ComputerDemo {
+    public static void main(String[] args) {
+
+
+       Task_29.Computer computer = new Task_29.Computer();
+       computer.turnOn();
+       computer.turnOff();
+        computer.whatColorIsComputer();
+    }
+ }
