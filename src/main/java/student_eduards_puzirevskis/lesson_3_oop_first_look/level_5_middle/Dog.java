f06a8d0514ca369abb1703113b8dec293554c447
eduards1911 <76965658+eduards1911@users.noreply.github.com>
2021-12-16 23:31:08 +0200

    HW_3_Eduards_Puzirevskis_upd

--- src/main/java/student_eduards_puzirevskis/lesson_3_oop_first_look/level_5_middle/Dog.java
+++ src/main/java/student_eduards_puzirevskis/lesson_3_oop_first_look/level_5_middle/Dog.java
@@ -1,35 +1,61 @@
 package student_eduards_puzirevskis.lesson_3_oop_first_look.level_5_middle;
-/*
-Тема: создание класса Dog.
 
-Создать класс собака (Dog)
-и продемонстрировать его работу с ним в программе.
-
-Бизнес требования:
-- У собаки должна быть кличка.
-- Кличку собаке дают в момент создания и потом ее изменить нельзя.
-- Собака должна уметь выполнять команду голос (voice()),
-при выполнении этой команды на консоль должно выводиться
-кличка собаки три раза.
-
-Создать отдельный класс DogDemo,
-объявить в нём main() метод и продемонстрировать
-использование класса собака (Dog).
- */
 class Dog {
    public String name;
    public int age;
+   public int dogNewAge;
+   public String color;
+
+    public Dog(String dogName){
+
+        this.name = dogName;
+
+    }
 
    public Dog(String dogName, int dogAge){
 
        this.name = dogName;
        this.age = dogAge;
-       if (dogAge < 1 ) {
-           System.out.println("Dog age can't be negative");
-       }
 
    }
-   public void voice() {
+
+    public Dog(String dogName, int dogAge, String color){
+
+        this.name = dogName;
+        this.age = dogAge;
+        this.color = color;
+
+    }
+
+ public void voice1() {
        System.out.println(this.name+ " ! " + this.name + " ! " + this.name + " ! ");
    }
+    public void voice2() {
+        System.out.println(this.name+ " ! " + this.name + " ! " + this.name + " ! ");
+        System.out.println("Dog's name is : " + this.name);
+        System.out.println("Dog's age is : " + this.age);
+    }
+
+    public int happyBirthday() {
+       this.dogNewAge = (age + 1);
+       return dogNewAge;
+
+    }
+    public void voice3() {
+        System.out.println(this.name+ " ! " + this.name + " ! " + this.name + " ! ");
+        System.out.println("Dog's name is : " + this.name);
+        System.out.println("Dog's age is : " + this.dogNewAge);
+    }
+    public void voice4() {
+        System.out.println(this.name+ " ! " + this.name + " ! " + this.name + " ! ");
+        System.out.println("Dog's name is : " + this.name);
+        System.out.println("Dog's age is : " + this.age);
+        System.out.println("Dog's color is : " + this.color);
+    }
+
+
+    public void changeColor(String newColor) {
+        this.color = newColor;
+    }
+
 }
