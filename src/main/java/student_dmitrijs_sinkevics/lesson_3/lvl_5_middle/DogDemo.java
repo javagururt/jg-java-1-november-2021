1f3f5840163a754d341b1dc3b9cffdf8f471fd0b
Dmitrijs-Sinkevics <dmitrijs.sinkevics@gmail.com>
2021-12-16 17:18:03 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/student_dmitrijs_sinkevics/lesson_1/lvl_4/Task11.java
    #	src/main/java/student_dmitrijs_sinkevics/lesson_2/lvl_1/Task1.java
    #	src/main/java/student_dmitrijs_sinkevics/lesson_2/lvl_1/Task2.java
    #	src/main/java/student_dmitrijs_sinkevics/lesson_2/lvl_2/Task8.java
    #	src/main/java/student_dmitrijs_sinkevics/lesson_2/lvl_3/Task10.java
    #	src/main/java/student_dmitrijs_sinkevics/lesson_2/lvl_3/Task11.java
    #	src/main/java/student_dmitrijs_sinkevics/lesson_2/lvl_3/Task9.java
    #	src/main/java/student_dmitrijs_sinkevics/lesson_2/lvl_5/Task18.java
    #	src/main/java/student_dmitrijs_sinkevics/lesson_2/lvl_6/Task23.java

--- src/main/java/student_dmitrijs_sinkevics/lesson_3/lvl_5_middle/DogDemo.java
+++ src/main/java/student_dmitrijs_sinkevics/lesson_3/lvl_5_middle/DogDemo.java
@@ -2,9 +2,10 @@ package student_dmitrijs_sinkevics.lesson_3.lvl_5_middle;
 
 public class DogDemo {
     public static void main(String[] args) {
-        Dog DogDemo=new Dog();
-
-        DogDemo.voice();
+        Dog DogDemo=new Dog("Brown");
+        DogDemo.changeColor("Black");
+        DogDemo.happyBirthday(); //increase age by 1
+        DogDemo.voice(); //greeting, age, color
 
 
     }
