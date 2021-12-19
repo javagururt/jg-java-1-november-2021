6c3c201f958ffa52320607a73e1dad8e20b93f8e
DmitrijT <di_riga@inbox.lv>
2021-12-19 14:46:22 +0200

    :)

---
+++ src/main/java/student_dima_talanov/lesson_4/level_2_intern/Task5.java
@@ -0,0 +1,22 @@
+package student_dima_talanov.lesson_4.level_2_intern;
+
+import java.util.Scanner;
+
+public class Task5 {
+
+    public static void main(String[] args) {
+        Scanner num = new Scanner(System.in);
+        int holeNumber1;
+        int holeNumber2;
+        System.out.println("Enter the hole number:");
+        holeNumber1 = num.nextInt();
+        holeNumber2 = num.nextInt();
+
+        if (holeNumber1 > holeNumber2) {
+            System.out.println(holeNumber1);
+
+        } else {
+            System.out.println(holeNumber2);
+        }
+    }
+}
