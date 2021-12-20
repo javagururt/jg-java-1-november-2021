0f51d7dda07ed90b599bb01f349cbbfab77fd7df
Jurij <jurijmardusevic@gmail.com>
2021-12-20 20:34:04 +0200

    Ups

--- src/main/java/student_jurij_mardusevic/lesson_4_if_statement/homework/level_2_intern/Task_5/TheLargestNumber.java
+++ src/main/java/student_jurij_mardusevic/lesson_4_if_statement/homework/level_2_intern/Task_5/TheLargestNumber.java
@@ -2,7 +2,7 @@ package student_jurij_mardusevic.lesson_4_if_statement.homework.level_2_intern.T
 
 import java.util.Scanner;
 
-public class TheLargestNumber {
+class TheLargestNumber {
 
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
