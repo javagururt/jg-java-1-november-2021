5804b358aad771bb356599327ccd1b76279ef837
Jurij <jurijmardusevic@gmail.com>
2021-12-20 16:12:38 +0200

    Ups

---
+++ src/main/java/student_jurij_mardusevic/lesson_4_if_statement/homework/level_1_intern/Number.java
@@ -0,0 +1,23 @@
+package student_jurij_mardusevic.lesson_4_if_statement.homework.level_1_intern;
+
+import java.util.Scanner;
+
+class Number<n> {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input a number: ");
+        int n = in.nextInt();
+        if (n>0);
+
+    {
+        System.out.println("Number is positive");
+    } else if (n<0)
+
+    {
+        System.out.println("Number is negative");
+    } else
+
+    {
+        System.out.println("Number is zero");
+    }
+
+}
