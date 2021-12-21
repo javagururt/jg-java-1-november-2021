b363aa1f28543f5b710dec09ec847a551b8d2bd8
RolandLV007 <darbameils@inbox.lv>
2021-12-21 09:15:41 +0200

    Merge remote-tracking branch 'origin/master'

--- src/main/java/student_rolands_gudels/lesson4_if_statement/level_4_junior/CalculatorTest.java
+++ src/main/java/student_rolands_gudels/lesson4_if_statement/level_4_junior/CalculatorTest.java
@@ -2,24 +2,73 @@ package student_rolands_gudels.lesson4_if_statement.level_4_junior;
 
 public class CalculatorTest {
 
-     /*   public static void main(String[] args) {
+        public static void main(String[] args) {
             CalculatorTest calculatorTest = new CalculatorTest();
             calculatorTest.sumTest();
-            // по аналогии реализуйте тесты для остальных методов
+            calculatorTest.substractionTest();
+            calculatorTest.divisionTest();
+            calculatorTest.multiplicationTest();
+            calculatorTest.isEvenTest();
         }
-
         public void sumTest() {
-            int firstNumber = 10;  // подготавливаем тестовые данные
-            int secondNumber = 5;  // подготавливаем тестовые данные
-            int expectedResult = 15;  // подготавливаем тестовые данные
-            Calculator calculator = new Calculator();
-            int realResult = calculator.sum(firstNumber, secondNumber);  // вызываем продакшен код, который хотим протестировать
-            if (realResult == expectedResult) {  // проверяем правильно ли сработал тестируемый код
+            int firstNumber = 10;
+            int secondNumber = 5;
+            int expectedResult = 15;
+            Task_11.Calculator calculator = new Task_11.Calculator();
+            int realResult = calculator.sum(firstNumber, secondNumber);
+            if (realResult == expectedResult) {
                 System.out.println("Sum test = OK");
             } else {
                 System.out.println("Sum test = FAIL");
             }
-        } */
+        }
+    public void substractionTest() {
+        int firstNumber = 10;
+        int secondNumber = 5;
+        int expectedResult = 5;
+        Task_11.Calculator calculator = new Task_11.Calculator();
+        int realResult = calculator.substraction(firstNumber, secondNumber);
+            if (realResult == expectedResult) {
+        System.out.println("Substraction test = OK");
+        } else {
+        System.out.println("Substraction test = FAIL");
+     }
+    }
+    public void divisionTest() {
+        int firstNumber = 10;
+        int secondNumber = 5;
+        int expectedResult = 2;
+        Task_11.Calculator calculator = new Task_11.Calculator();
+        int realResult = calculator.division(firstNumber, secondNumber);
+        if (realResult == expectedResult) {
+            System.out.println("Division test = OK");
+        } else {
+            System.out.println("Division test = FAIL");
+        }
+    }
+    public void multiplicationTest() {
+        int firstNumber = 10;
+        int secondNumber = 5;
+        int expectedResult = 50;
+        Task_11.Calculator calculator = new Task_11.Calculator();
+        int realResult = calculator.multiplication(firstNumber, secondNumber);
+        if (realResult == expectedResult) {
+            System.out.println("Multiplication test = OK");
+        } else {
+            System.out.println("Multiplication test = FAIL");
+        }
+    }
+    public void isEvenTest() {
+        int number = 10;
+        Task_11.Calculator calculator = new Task_11.Calculator();
+        boolean realResult = calculator.isEven(number);
+        if (realResult == true) {
+            System.out.println("isEven test = OK");
+        } else {
+            System.out.println("isEven test = FAIL");
+        }
+    }
+
 
     }
 
--- src/main/java/student_rolands_gudels/lesson4_if_statement/level_4_junior/Task_13.java
+++ src/main/java/student_rolands_gudels/lesson4_if_statement/level_4_junior/Task_13.java
@@ -17,4 +17,5 @@ class Calculator {
     }
 
 }*/
+    //Done
 }
