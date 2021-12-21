5131ab5aa5a27e3c4c380025cefe7877572225d0
DmitrijT <di_riga@inbox.lv>
2021-12-21 18:39:37 +0200

    :)

---
+++ src/main/java/student_dima_talanov/lesson_4/level_4_junior/Calculator.java
@@ -0,0 +1,34 @@
+package student_dima_talanov.lesson_4.level_4_junior;
+
+public class Calculator {
+
+    public int sum(int firstNumber, int secondNumber) {
+        return firstNumber + secondNumber;
+    }
+
+    public int subtraction(int firstNumber, int secondNumber) {
+        return firstNumber - secondNumber;
+    }
+
+    public int multiplication(int firstNumber, int secondNumber) {
+        return firstNumber * secondNumber;
+    }
+
+    public int division(int firstNumber, int secondNumber) {
+        return firstNumber / secondNumber;
+    }
+
+    public int parity(int firstNumber) {
+        return firstNumber % 2;
+    }
+
+    public int maxOrEqualOfTwoNumbers(int firstNumber, int secondNumber) {
+        if (firstNumber > secondNumber) {
+            return firstNumber;
+        } else if (secondNumber > firstNumber) {
+            return secondNumber;
+        } else {
+            return secondNumber | firstNumber;
+        }
+    }
+}
