c1cbe8115f0d35f47efa763f65423b8643aecfef
RolandLV007 <darbameils@inbox.lv>
2021-12-23 09:02:32 +0200

    Merge remote-tracking branch 'origin/master'

--- src/main/java/student_rolands_gudels/lesson4_if_statement/level_6_middle/StockTest.java
+++ src/main/java/student_rolands_gudels/lesson4_if_statement/level_6_middle/StockTest.java
@@ -11,6 +11,12 @@ class StockTest {
              stockTest.maxTestFour();
              stockTest.maxTestFive();
              stockTest.maxTestSix();
+             stockTest.minTestOne();
+             stockTest.minTestTwo();
+             stockTest.minTestThree();
+             stockTest.minTestFour();
+             stockTest.minTestFive();
+             stockTest.minTestSix();
 
      }
 
@@ -78,6 +84,69 @@ class StockTest {
         if (Objects.equals(expectedResult, stock.maximalPrice)) {
             System.out.println(" Max test six = OK");
         } else { System.out.println("Max five six = FAIL");
+        }
+    }
+
+    //
+
+    public void minTestOne() {
+        int expectedResult = 999;
+        Stock stock = new Stock("Test1",999);
+        if (Objects.equals(expectedResult, stock.minimalPrice)) {
+            System.out.println(" Min test One = OK");
+        } else { System.out.println("Min test One = FAIL");
+        }
+    }
+    public void minTestTwo() {
+        int expectedResult = 1;
+        Stock stock = new Stock("Test1",999);
+        stock.updatePrice(1);
+        if (Objects.equals(expectedResult, stock.minimalPrice)) {
+            System.out.println(" Min test Two = OK");
+        } else { System.out.println("Min test Two = FAIL");
+        }
+    }
+    public void minTestThree() {
+        int expectedResult = 2;
+        Stock stock = new Stock("Test1",999);
+        stock.updatePrice(2);
+        stock.updatePrice(1000);
+        if (Objects.equals(expectedResult, stock.minimalPrice)) {
+            System.out.println(" Min test Three = OK");
+        } else { System.out.println("Min test Three = FAIL");
+        }
+    }
+    public void minTestFour() {
+        int expectedResult = 2;
+        Stock stock = new Stock("Test1",999);
+        stock.updatePrice(2);
+        stock.updatePrice(1000);
+        stock.updatePrice(8);
+        if (Objects.equals(expectedResult, stock.minimalPrice)) {
+            System.out.println(" Min test four = OK");
+        } else { System.out.println("Min four Three = FAIL");
+        }
+    }
+    public void minTestFive() {
+        int expectedResult = 9;
+        Stock stock = new Stock("Test1",9);
+        stock.updatePrice(27);
+        if (Objects.equals(expectedResult, stock.minimalPrice)) {
+            System.out.println(" Min test five = OK");
+        } else { System.out.println("Min five five = FAIL");
+        }
+    }
+    public void minTestSix() {
+        int expectedResult = 5;
+        Stock stock = new Stock("Test1",10);
+        stock.updatePrice(12);
+        stock.updatePrice(5);
+        stock.updatePrice(7);
+        stock.updatePrice(99);
+        stock.updatePrice(77);
+        if (Objects.equals(expectedResult, stock.minimalPrice)) {
+            System.out.println(" Min test six = OK");
+        } else { System.out.println("Min five six = FAIL");
         }
     }
 }
