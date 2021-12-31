ffdf48444a405bc192d09f8fa22b0d82fe7ec476
RolandLV007 <darbameils@inbox.lv>
2021-12-31 18:30:22 +0200

    Merge remote-tracking branch 'origin/master'

--- src/main/java/student_rolands_gudels/lesson_5_arrays_for_loop/level_4_junior/Task_28.java
+++ src/main/java/student_rolands_gudels/lesson_5_arrays_for_loop/level_4_junior/Task_28.java
@@ -1,10 +1,28 @@
 package student_rolands_gudels.lesson_5_arrays_for_loop.level_4_junior;
 
-public class Task_28 {
+import java.util.Random;
+import java.util.Arrays;
+import java.util.OptionalInt;
+import java.util.stream.IntStream;
 
+public class Task_28 {
+    public static void main(String[] args) {
 /*    Напишите программу, в которой:
             - создайте массив произвольной длины
 - заполните массив случайными числами
 - распечатайте на консоль все элементы массива
 - найдите наименьшее число в массиве и выведети его на консоль.*/
+        Random random = new Random();
+        random.nextInt(10);
+        int x = random.nextInt(10);
+        int[] numbers = new int[x];
+        for (int i=0;i<numbers.length;i++){
+            numbers[i]= random.nextInt(10)+1;
+            System.out.println(numbers[i]);
+        }
+        IntStream intStream = Arrays.stream(numbers);
+        OptionalInt optionalInt = intStream.min();
+        int minAsInt = optionalInt.getAsInt();
+        System.out.println("Minimal number = " + minAsInt);
+    }
 }
--- src/main/java/student_rolands_gudels/lesson_5_arrays_for_loop/level_4_junior/Task_30.java
+++ src/main/java/student_rolands_gudels/lesson_5_arrays_for_loop/level_4_junior/Task_30.java
@@ -1,10 +1,26 @@
 package student_rolands_gudels.lesson_5_arrays_for_loop.level_4_junior;
 
-public class Task_30 {
+import java.util.Random;
 
+public class Task_30 {
+    public static void main(String[] args) {
 /*    Напишите программу, в которой:
             - создайте массив произвольной длины
 - заполните массив случайными числами
 - распечатайте на консоль все элементы массива
 - найдите все нечётные числа в массиве и выведети их на консоль.*/
-}
+        Random random = new Random();
+        random.nextInt();
+        int x = random.nextInt(10);
+        int[] numbers = new int[x];
+        for (int i=0;i<numbers.length;i++) {
+            numbers[i] = random.nextInt(10) + 1;
+            System.out.println(numbers[i]);
+        }
+        for (int j=0;j<numbers.length;j++) {
+            if (numbers[j] % 2 != 0)
+                System.out.println("All arays odd numbers are: " + numbers[j]);
+        }
+        }
+    }
+
