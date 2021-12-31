a63c77fc02531e5667f84bbaf9ddae3a59dde6a0
RolandLV007 <darbameils@inbox.lv>
2021-12-31 13:23:51 +0200

    Merge remote-tracking branch 'origin/master'

--- src/main/java/student_andrei_karamnov/lesson_5_arrays_for_loop/level_5_middle/ArrayUtil.java
+++ src/main/java/student_andrei_karamnov/lesson_5_arrays_for_loop/level_5_middle/ArrayUtil.java
@@ -1,10 +1,10 @@
 package student_andrei_karamnov.lesson_5_arrays_for_loop.level_5_middle;
 
 class ArrayUtil {
-    public int[] createArray(int arrayLength) {
+   /* public int[] createArray(int arrayLength) {
 
     }
     public void fillArrayWithRandomNumbers(int[] array) {
         // Напишите реализацию !!!
-    }
+    }*/
 }
