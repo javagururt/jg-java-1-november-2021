959d6f249ba2814f8e6afcab5d2b66ece18e2e45
DmitrijT <di_riga@inbox.lv>
2022-01-01 20:47:47 +0200

    :)

--- src/main/java/student_dima_talanov/lesson_5/level_2_intern/Task12.java
+++ src/main/java/student_dima_talanov/lesson_5/level_2_intern/Task12.java
@@ -6,7 +6,7 @@ public class Task12 {
     public static void main(String[] args) {
         int[] myArray = new int[3];
         for (int i = 0; i < 3; i++) {
-            myArray[i] = (int) (Math.random() * 5);
+            myArray[i] = (int) (Math.random() * 8);
             System.out.println(myArray[i]);
         }
     }
