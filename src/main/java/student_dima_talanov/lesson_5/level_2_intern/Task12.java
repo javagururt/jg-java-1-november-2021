a83d184a75f1b1a8ddbb5962a980471c66219862
DmitrijT <di_riga@inbox.lv>
2022-01-01 20:45:53 +0200

    :)

---
+++ src/main/java/student_dima_talanov/lesson_5/level_2_intern/Task12.java
@@ -0,0 +1,16 @@
+package student_dima_talanov.lesson_5.level_2_intern;
+
+
+public class Task12 {
+
+    public static void main(String[] args) {
+        int[] myArray = new int[3];
+        for (int i = 0; i < 3; i++) {
+            myArray[i] = (int) (Math.random() * 5);
+            System.out.println(myArray[i]);
+        }
+    }
+}
+
+
+
