1149590529cad71cb10bda8d9740cca420aee33c
mihailsnikolajevs <mike2309@inbox.lv>
2022-01-03 14:08:37 +0200

    HomeWorkTask4ClassPhone
    Signed-off-by: mihailsnikolajevs <mike2309@inbox.lv>

---
+++ src/main/java/student_mihails_nikolajevs/BankAccount.java
@@ -0,0 +1,10 @@
+package student_mihails_nikolajevs;
+/*Дан код, нужно исправить ошибку компиляции.
+        Для сдачи работы создайте исправленный класс.
+*/
+class BankAccount {
+
+    public static void main(String[] args) {
+
+    }
+}
