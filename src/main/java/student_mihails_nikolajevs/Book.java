7089ad1103125a36279841472cb06118ba4d04ce
mihailsnikolajevs <mike2309@inbox.lv>
2022-01-03 14:03:26 +0200

    HomeWorkTask3ClassBook
    Signed-off-by: mihailsnikolajevs <mike2309@inbox.lv>

---
+++ src/main/java/student_mihails_nikolajevs/Book.java
@@ -0,0 +1,13 @@
+package student_mihails_nikolajevs;
+/*Дан код, нужно исправить ошибку компиляции.
+Для сдачи работы создайте исправленный класс.
+
+class Book {
+
+ */
+
+public class Book {
+    public static void main(String[] args) {
+        
+    }
+}
