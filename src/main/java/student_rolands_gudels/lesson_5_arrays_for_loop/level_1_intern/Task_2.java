52781c57462f57c8bdea675f56fe3612d8767f72
RolandLV007 <darbameils@inbox.lv>
2022-01-04 18:02:55 +0200

    Merge remote-tracking branch 'origin/master'

--- src/main/java/student_rolands_gudels/lesson_5_arrays_for_loop/level_1_intern/Task_2.java
+++ src/main/java/student_rolands_gudels/lesson_5_arrays_for_loop/level_1_intern/Task_2.java
@@ -1,7 +1,7 @@
 package student_rolands_gudels.lesson_5_arrays_for_loop.level_1_intern;
 
  class Task_2 {
- static    class ArrayDefinition2 {
+ static class ArrayDefinition2 {
 
         public static void main(String[] args) {
             byte[] numbers = new byte[10];
