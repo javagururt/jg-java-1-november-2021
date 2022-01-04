f67b0f7211daf45d7ad4b3838e88319d1391f86c
hq4proxd <salamotkapitanov@gmail.com>
2022-01-04 18:14:31 +0200

    -_-

--- src/main/java/student_victor_veselov/lesson_4/Task9.java
+++ src/main/java/student_victor_veselov/lesson_4/Task9.java
@@ -1,8 +1,32 @@
 package student_victor_veselov.lesson_4;
 
+import java.util.Scanner;
+
 public class Task9 {
 
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("1st");
+        int a = scanner.nextInt();
+        System.out.println("2nd");
+        int b = scanner.nextInt();
+        System.out.println("3rd");
+        int c = scanner.nextInt();
+
+        if (a < b && b < c){
+            System.out.println("Numbers sorted " + a + " " + b + " " + c);
+        }
+        if (a < b && b > c){
+            System.out.println("Numbers sorted " + a + " " + c + " " + b);
+        }
+        if (a > b && b > c){
+            System.out.println("Numbers sorted " + c + " " + b + " " + a);
+        }
+        if (a > b && b < c){
+            System.out.println("Numbers sorted " + b + " " + a + " " + c);
+        }
+
 
 
     }
