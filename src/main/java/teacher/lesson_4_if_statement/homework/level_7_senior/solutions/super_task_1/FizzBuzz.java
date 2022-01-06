738142ce31c140d8e6138c811e2d13e9c58c4ad9
Ruslans Tufetulovs <ruslans.tufetulovs@gmail.com>
2022-01-06 22:20:46 +0200

    lesson 6

--- src/main/java/teacher/lesson_4_if_statement/homework/level_7_senior/solutions/super_task_1/FizzBuzz.java
+++ src/main/java/teacher/lesson_4_if_statement/homework/level_7_senior/solutions/super_task_1/FizzBuzz.java
@@ -2,24 +2,24 @@ package teacher.lesson_4_if_statement.homework.level_7_senior.solutions.super_ta
 
 public class FizzBuzz {
 
-    public String detect(int number){
+    public String detect(int number) {
         if (isDividedBy3(number) && isDividedBy5(number)) {
-			return "FizzBuzz";
-		}
-		if(isDividedBy3(number)) {
-			return "Fizz";
-		}
-		if(isDividedBy5(number)) {
-			return "Buzz";
-		}
-		return "" + number;
+            return "FizzBuzz";
+        }
+        if (isDividedBy3(number)) {
+            return "Fizz";
+        }
+        if (isDividedBy5(number)) {
+            return "Buzz";
+        }
+        return "" + number;
     }
 
     private boolean isDividedBy3(int number) {
-    	return number % 3 == 0;
-	}
+        return number % 3 == 0;
+    }
 
-	private boolean isDividedBy5(int number) {
-		return number % 5 == 0;
-	}
+    private boolean isDividedBy5(int number) {
+        return number % 5 == 0;
+    }
 }
---
+++ src/main/java/teacher/lesson_6_arrays_while_loop/lessoncode/Person.java
@@ -0,0 +1,15 @@
+package teacher.lesson_6_arrays_while_loop.lessoncode;
+
+class Person {
+
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
