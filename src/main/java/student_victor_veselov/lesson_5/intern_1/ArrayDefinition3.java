1c800c400f6ceb1f2453428bb7d23f5b520294c2
hq4proxd <salamotkapitanov@gmail.com>
2022-01-07 21:57:24 +0200

    -_-

---
+++ src/main/java/student_victor_veselov/lesson_5/intern_1/ArrayDefinition3.java
@@ -0,0 +1,10 @@
+package student_victor_veselov.lesson_5.intern_1;
+
+public class ArrayDefinition3 {
+
+
+    public static void main(String[] args) {
+        int[] numbers = new int[10];
+
+    }
+}
---
+++ src/main/java/student_victor_veselov/lesson_5/intern_2/Task10.java
@@ -0,0 +1,12 @@
+package student_victor_veselov.lesson_5.intern_2;
+
+import java.util.Arrays;
+
+public class Task10 {
+
+    public static void main(String[] args) {
+
+        int[] numbers = new int[]{ 3,4,5};
+        System.out.println(Arrays.toString(numbers));
+    }
+}
