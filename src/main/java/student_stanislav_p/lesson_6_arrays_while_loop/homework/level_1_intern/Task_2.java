3a6e1779c04373f868c6acc5310a6f7833898925
StanislavP <Perminovsb@mail.ee>
2022-01-08 00:29:53 +0200

    Lesson 5

---
+++ src/main/java/student_stanislav_p/lesson_6_arrays_while_loop/homework/level_1_intern/Task_2.java
@@ -0,0 +1,35 @@
+package student_stanislav_p.lesson_6_arrays_while_loop.homework.level_1_intern;
+
+
+/*
+Найдите ошибку в программе и исправить её:
+
+class WhileDefinition2 {
+
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 100)
+            System.out.println(i);
+            i++;
+        }
+    }
+}
+
+ */
+
+
+public class Task_2 {
+
+    static class WhileDefinition2 {
+
+        public static void main(String[] args) {
+
+
+            int i = 1;
+            while (i <= 100) {
+                System.out.println(i);
+                i++;
+            }
+        }
+    }
+}
---
+++ src/main/java/student_stanislav_p/lesson_6_arrays_while_loop/homework/level_1_intern/Task_5.java
@@ -0,0 +1,34 @@
+package student_stanislav_p.lesson_6_arrays_while_loop.homework.level_1_intern;
+
+/*
+Найдите логическую ошибку в программе и исправить её.
+Программа должна выводить числа от 1 до 100 на консоль.
+
+class WhileDefinition5 {
+
+    public static void main(String[] args) {
+        int i = 1;
+        while (i >= 100) {
+            System.out.println(i);
+            i--;
+        }
+    }
+
+}
+
+ */
+
+public class Task_5 {
+
+    static class WhileDefinition5 {
+
+        public static void main(String[] args) {
+            int i = 1;
+            while (i <= 100) {
+                System.out.println(i);
+                i++;
+            }
+        }
+
+    }
+}
