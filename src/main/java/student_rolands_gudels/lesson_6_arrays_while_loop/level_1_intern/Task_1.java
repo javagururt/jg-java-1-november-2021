a525cf2dc6aa24ccec53418b459594d978e92921
RolandLV007 <darbameils@inbox.lv>
2022-01-07 22:05:52 +0200

    Merge remote-tracking branch 'origin/master'

---
+++ src/main/java/student_rolands_gudels/lesson_6_arrays_while_loop/level_1_intern/Task_1.java
@@ -0,0 +1,17 @@
+package student_rolands_gudels.lesson_6_arrays_while_loop.level_1_intern;
+
+public class Task_1 {
+    /*Найдите ошибку в программе и исправить её:
+
+    class WhileDefinition1 {
+
+        public static void main(String[] args) {
+            int i = 1;
+            while i <= 100) {
+                System.out.println(i);
+                i++;
+            }
+        }
+
+    }*/
+}
