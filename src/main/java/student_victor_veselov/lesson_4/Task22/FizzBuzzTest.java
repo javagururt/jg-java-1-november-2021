3e46de56de3eb71b6d39e5ce045662bc28fe18f5
hq4proxd <salamotkapitanov@gmail.com>
2022-01-09 16:18:32 +0200

    -_-

---
+++ src/main/java/student_victor_veselov/lesson_4/Task22/FizzBuzzTest.java
@@ -0,0 +1,9 @@
+package student_victor_veselov.lesson_4.Task22;
+
+public class FizzBuzzTest {
+
+    public static void main(String[] args) {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        fizzBuzz.detect(9);
+    }
+}
