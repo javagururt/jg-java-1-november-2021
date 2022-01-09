1dd9481d0966a3e4045de277a6ca30857b2b885c
eduards1911 <76965658+eduards1911@users.noreply.github.com>
2022-01-09 17:41:15 +0200

    HW_Eduards_Puzirevskis

---
+++ src/main/java/student_eduards_puzirevskis/lesson_6_arrays_while_loop/level_3_junior/ArrayServiceDemo.java
@@ -0,0 +1,19 @@
+package student_eduards_puzirevskis.lesson_6_arrays_while_loop.level_3_junior;
+
+ class ArrayServiceDemo {
+
+     public static void main(String[] args) {
+         ArrayService arrayService = new ArrayService();
+         int size = arrayService.getOurArraySize();
+         int[] numbers = arrayService.createArray(size);
+         arrayService.fillArrayWithRandomNumbers(numbers);
+         arrayService.printArrayToConsole(numbers);
+         int numberToFind = arrayService.whatNumberWeAreLookingFor();
+         int countOccurrences = arrayService.countOccurrences(numbers, numberToFind);
+         boolean searchingResult = arrayService.findingMatchInArray(numbers, numberToFind);
+         arrayService.printSearchingResult(numberToFind,searchingResult, countOccurrences);
+         boolean replacing = arrayService.replaceNumber(numbers, arrayService.getNumberToReplace(), arrayService.getNewNumberToInsert());
+         arrayService.printReplacingResults(replacing);
+         arrayService.printArrayToConsole(numbers);
+     }
+}
