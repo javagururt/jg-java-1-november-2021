a34797f694ea289b387d293f1f56f6280b945674
Jurij <jurijmardusevic@gmail.com>
2022-01-11 10:00:23 +0200

    array

---
+++ src/main/java/student_jurij_mardusevic/lesson_5_arrays_for_loop/homework/level_3_junior/Task_19/ForLoopDefinition4.java
@@ -0,0 +1,9 @@
+package student_jurij_mardusevic.lesson_5_arrays_for_loop.homework.level_3_junior.Task_19;
+
+class ForLoopDefinition4 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
