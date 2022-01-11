640cd902d400c2baacf58a36913d4c4ef781bfb7
DmitrijT <di_riga@inbox.lv>
2022-01-11 23:36:32 +0200

    :)

--- src/main/java/student_dima_talanov/lesson_6/level_2_intern/DayOfTheWeekDetectorDemo.java
+++ src/main/java/student_dima_talanov/lesson_6/level_2_intern/DayOfTheWeekDetectorDemo.java
@@ -5,15 +5,7 @@ public class DayOfTheWeekDetectorDemo {
     public static void main(String[] args) {
         DayOfTheWeekDetector detector = new DayOfTheWeekDetector();
         int dayNumber = detector.getDayNumberFromUser();
-
-        // switch (firstNumber) {
-        //    case 1 -> System.out.println("Monday");
-        //    case 2 -> System.out.println("Tuesday");
-        //     case 3 -> System.out.println("Wednesday");
-        //     case 4 -> System.out.println("Thursday");
-        //     case 5 -> System.out.println("Friday");
-        //     case 6 -> System.out.println("Saturday");
-        //      case 7 -> System.out.println("Sunday");
-        //     default -> System.out.println("Not correct day number");
+        String dayOfTheWeek = detector.findDayOfTheWeek(dayNumber);
+        System.out.println(dayOfTheWeek);
     }
 }
