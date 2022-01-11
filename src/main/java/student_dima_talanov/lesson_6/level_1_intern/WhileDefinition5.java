124532eef1dc363bc9c58d1d133d0ff838ec119f
DmitrijT <di_riga@inbox.lv>
2022-01-11 21:21:07 +0200

    :)

---
+++ src/main/java/student_dima_talanov/lesson_6/level_1_intern/WhileDefinition5.java
@@ -0,0 +1,12 @@
+package student_dima_talanov.lesson_6.level_1_intern;
+
+public class WhileDefinition5 {
+
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 100) {
+            System.out.print(" " + i + " ");
+            i++;
+        }
+    }
+}
