487276ca3e29c9c66902306ad97940473b3372ac
RolandLV007 <darbameils@inbox.lv>
2022-01-11 18:10:08 +0200

    Merge remote-tracking branch 'origin/master'

--- src/main/java/student_rolands_gudels/lesson_6_arrays_while_loop/level_4_junior/Task_18.java
+++ src/main/java/student_rolands_gudels/lesson_6_arrays_while_loop/level_4_junior/Task_18.java
@@ -1,9 +1,32 @@
 package student_rolands_gudels.lesson_6_arrays_while_loop.level_4_junior;
 
 public class Task_18 {
-   /* Создайте класс WhileInfiniteLoop и в нём напишите бесконечный
-    цикл при помощи цикла while.
 
-    Создайте класс ForInfiniteLoop и в нём напишите бесконечный
+    public static void main(String[] args) {
+        Task_18 task_18 = new Task_18();
+        //task_18.WhileInfiniteLoop();
+       // task_18.ForInfiniteLoop();
+    }
+
+    public static void WhileInfiniteLoop() {
+        boolean bad = true;
+        while (bad) {
+            System.out.println("b");
+        }
+    }
+
+    public static void WhileInfiniteLoopBreak(){
+
+    }
+
+
+            /*Создайте класс и в нём напишите бесконечный
+    цикл при помощи цикла while.*/
+
+    public void ForInfiniteLoop() {
+        for (int i = 0; i <= 0; ) {
+            System.out.println("test");
+        }
+    }/*Создайте класс  и в нём напишите бесконечный
     цикл при помощи цикла for.*/
 }
