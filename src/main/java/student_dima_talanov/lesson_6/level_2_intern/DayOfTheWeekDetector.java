2a954806e35455e6ae2bdb8bce32a77a89745b9a
DmitrijT <di_riga@inbox.lv>
2022-01-11 21:19:44 +0200

    :)

---
+++ src/main/java/student_dima_talanov/lesson_6/level_2_intern/DayOfTheWeekDetector.java
@@ -0,0 +1,17 @@
+package student_dima_talanov.lesson_6.level_2_intern;
+
+import java.util.Scanner;
+
+public class DayOfTheWeekDetector {
+
+    public int getDayNumberFromUser() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input day number between 1 and 7: ");
+        return scanner.nextInt();
+
+        //public String findDayOfTheWeek ( int dayNumber){
+    }
+}
+
+
+
