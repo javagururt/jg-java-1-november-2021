fe6d993fc51bea4dc48bc0096115cddbfe01e070
mihailsnikolajevs <mike2309@inbox.lv>
2022-01-12 19:41:01 +0200

    Lesson3_task1_robot

---
+++ src/main/java/level_1_intern_/Robot.java
@@ -0,0 +1,30 @@
+package level_1_intern_;
+/*
+
+Создайте класс робот (Robot).
+
+Научите вашего робота "говорить" привет.
+
+Создайте в классе Robot метод со следующей сигнатурой:
+
+    public void sayHello() {
+        // тут напишите команду распечатки на консоли строки "Hello!"
+    }
+
+
+
+ */
+public class Robot {
+    String name;
+    public void sayHello() {
+        System.out.println("Hello");
+
+
+
+
+
+
+
+    }
+
+}
