45a224b622cacb8e03712f2cce28465996459812
DmitrijT <di_riga@inbox.lv>
2022-01-13 01:16:00 +0200

    :)

---
+++ src/main/java/student_dima_talanov/lesson_6/level_3_junior/Task12.java
@@ -0,0 +1,22 @@
+package student_dima_talanov.lesson_6.level_3_junior;
+
+public class Task12 {
+
+    public static void main(String[] args) {
+        int[] numberArray = {2, 8, 16, 20, 24, 8, 8, 8};
+        int myNumber = 2;
+        System.out.println("Result is: " + contains(numberArray, myNumber));
+
+    }
+
+    static boolean contains(int[] arr, int numberToSearch) {
+        for (int value : arr) {
+            if (value == numberToSearch)
+                return true;
+        }
+        return false;
+    }
+}
+
+
+
---
+++ src/main/java/student_dima_talanov/lesson_6/level_3_junior/Task13.java
@@ -0,0 +1,19 @@
+package student_dima_talanov.lesson_6.level_3_junior;
+
+public class Task13 {
+
+    public static void main(String[] args) {
+        int[] numberArray = {2, 8, 16, 20, 24, 8, 8, 8};
+        int myNumber = 2;
+        System.out.println("Number array is: " + countOccurrences(numberArray, myNumber));
+    }
+    static int countOccurrences(int[] arr, int numberToSearch) {
+        int count = 0;
+        for (int value : arr) {
+            if (value == numberToSearch) ;
+            count++;
+        }
+        return count++;
+    }
+}
+
