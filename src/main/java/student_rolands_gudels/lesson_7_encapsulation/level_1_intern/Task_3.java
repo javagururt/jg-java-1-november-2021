5df06a9bcb68d035d5c53c4f1cb2a461b45ff2cc
RolandLV007 <darbameils@inbox.lv>
2022-01-14 13:33:05 +0200

    Merge remote-tracking branch 'origin/master'

---
+++ src/main/java/student_rolands_gudels/lesson_7_encapsulation/level_1_intern/Task_3.java
@@ -0,0 +1,8 @@
+package student_rolands_gudels.lesson_7_encapsulation.level_1_intern;
+
+public class Task_3 {
+   /* Из подзадач запрограммированных в Task_2.txt
+    соберите решение главной задачи из Task_1.txt.
+
+    В классе WordServiceTest напишите тесты для всей задачи целиком.*/
+}
---
+++ src/main/java/student_rolands_gudels/lesson_7_encapsulation/level_7_senior/Task_15.java
@@ -0,0 +1,10 @@
+package student_rolands_gudels.lesson_7_encapsulation.level_7_senior;
+
+public class Task_15 {
+/*    Создать класс UserEntity с учетом, что вы уже знаете, что такое инкапсюляция.
+    У класса UserEntity должны быть поля:
+            - id
+  - имя
+  - фамилия
+  - персональный код*/
+}
