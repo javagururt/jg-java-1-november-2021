9e9db1b03b8b93351820f8560e9518c7cd6947fd
StanislavP <Perminovsb@mail.ee>
2022-01-16 03:47:46 +0200

    Lesson 7

---
+++ src/main/java/student_stanislav_p/lesson_7_encapsulation/homework/level_5_middle/PowerCalculator.java
@@ -0,0 +1,18 @@
+package student_stanislav_p.lesson_7_encapsulation.homework.level_5_middle;
+
+class PowerCalculator {
+
+    double multiPower(double firstNumber, int secondNumber) {
+
+        double result;
+
+        if (secondNumber==0) {result = 1;} else result=firstNumber;
+
+        for (int i = 1; i < secondNumber; i++) {
+
+            result=result*firstNumber;
+        }
+        return result;
+    }
+
+}
