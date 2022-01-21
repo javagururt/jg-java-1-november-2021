5e6327ece0c3018ef9a1a52cef6c0b0095e382ef
StanislavP <Perminovsb@mail.ee>
2022-01-21 19:48:55 +0200

    Lesson 8

---
+++ src/main/java/student_stanislav_p/lesson_8_inheritance/homework/level_3_junior/Task11.java
@@ -0,0 +1,15 @@
+package student_stanislav_p.lesson_8_inheritance.homework.level_3_junior;
+
+/*
+Исправьте код программы так, что бы он компилировался.
+
+abstracts class LoanSystem {
+
+}
+ */
+
+abstract class LoanSystem {
+
+}
+public class Task11 {
+}
--- src/main/java/student_stanislav_p/lesson_8_inheritance/homework/level_3_junior/Task14.java
+++ src/main/java/student_stanislav_p/lesson_8_inheritance/homework/level_3_junior/Task14.java
@@ -1,3 +1,7 @@
+package student_stanislav_p.lesson_8_inheritance.homework.level_3_junior;
+
+
+/*
 Исправьте код программы так, что бы он компилировался.
 Класс Car менять нельзя.
 Классы ElectricCar и PetrolCar должны наследоваться
@@ -21,4 +25,35 @@ class PetrolCar extends Car {
 
     private double petrolTankCapacity;
 
+}
+ */
+
+
+abstract class Car {
+
+    abstract void accelerate();
+
+}
+
+class ElectricCar extends Car {
+
+
+    private int batteryCharge;
+
+    @Override
+    void accelerate() {}
+
+
+}
+
+class PetrolCar extends Car {
+
+    private double petrolTankCapacity;
+
+    @Override
+    void accelerate() {}
+
+}
+
+public class Task14 {
 }
