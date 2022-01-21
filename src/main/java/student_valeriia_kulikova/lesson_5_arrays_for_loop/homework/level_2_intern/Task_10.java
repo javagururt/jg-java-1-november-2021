f938fa55ca318c1611f724915035c502f7126d60
ValeriaKul <lera.dribnyuk@mail.ru>
2022-01-21 14:51:42 +0200

    ;)

--- src/main/java/student_valeriia_kulikova/lesson_5_arrays_for_loop/homework/level_2_intern/Task_10.java
+++ src/main/java/student_valeriia_kulikova/lesson_5_arrays_for_loop/homework/level_2_intern/Task_10.java
@@ -3,11 +3,7 @@ package student_valeriia_kulikova.lesson_5_arrays_for_loop.homework.level_2_inte
 import java.util.Arrays;
 
 public class Task_10 {
-    /*Напишите программу, в которой:
- - создайте массив целых чисел длиной 3,
- - заполните каждую ячейку массива любым целым числом,
- - выведите значение каждой ячейки на консоль.
-*/
+
     static class ArrayOfNumbers {
         public static void main(String[] args) {
             int [] arrayOfNumbers = new int [3];
