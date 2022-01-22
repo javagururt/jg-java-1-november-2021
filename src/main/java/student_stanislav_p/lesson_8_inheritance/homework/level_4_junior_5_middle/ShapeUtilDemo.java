2c1b931a1feefe9e0629194281e9df31451c507a
StanislavP <Perminovsb@mail.ee>
2022-01-23 01:37:45 +0200

    Lesson 8

--- src/main/java/student_stanislav_p/lesson_8_inheritance/homework/level_4_junior_5_middle/ShapeUtilDemo.java
+++ src/main/java/student_stanislav_p/lesson_8_inheritance/homework/level_4_junior_5_middle/ShapeUtilDemo.java
@@ -19,5 +19,17 @@ package student_stanislav_p.lesson_8_inheritance.homework.level_4_junior_5_middl
 
 public class ShapeUtilDemo {
 
+    public static void main(String[] args) {
+
+        ShapeUtil shapeUtil = new ShapeUtil();
+        Shape circle = shapeUtil.createRandomCircle();
+        double circleArea = shapeUtil.calculateArea(circle);
+        double circlePerimeter = shapeUtil.calculatePerimeter(circle);
+
+        System.out.println("Circles area = " + circleArea);
+        System.out.println();
+        System.out.println("Circles perimeter = " + circlePerimeter);
+    }
+
 
 }
