76ed52c6c3c6ff75bb75622ca39f371379b23ae2
StanislavP <Perminovsb@mail.ee>
2022-01-23 18:23:40 +0200

    Lesson 8

---
+++ src/main/java/student_stanislav_p/lesson_8_inheritance/homework/level_7_senior/library/Author.java
@@ -0,0 +1,7 @@
+package student_stanislav_p.lesson_8_inheritance.homework.level_7_senior.library;
+
+class Author {
+    int indexAuthor;
+    String  Author;
+
+}
---
+++ src/main/java/student_stanislav_p/lesson_8_inheritance/homework/level_7_senior/library/Catalog.java
@@ -0,0 +1,13 @@
+package student_stanislav_p.lesson_8_inheritance.homework.level_7_senior.library;
+
+
+
+public class Catalog {
+    private int catalogNumber;
+    private int authorIndex;
+    private String [] nameOfTheBook;
+
+
+
+
+}
