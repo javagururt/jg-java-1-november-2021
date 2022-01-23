d1cc201658e6063838084b6382c6ac7973e440f5
StanislavP <Perminovsb@mail.ee>
2022-01-23 22:01:12 +0200

    Lesson 8

---
+++ src/main/java/student_stanislav_p/lesson_8_inheritance/homework/level_7_senior/library/Book.java
@@ -0,0 +1,38 @@
+package student_stanislav_p.lesson_8_inheritance.homework.level_7_senior.library;
+
+
+class Book {
+    private String author;
+    private String title;
+    private int year;
+    private int pages;
+
+    Book(String author, String title, int year, int pages){
+        this.author = author;
+        this.title = title;
+        this.year = year;
+        this.pages = pages;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    @Override
+    public String toString(){
+        return author + ". \"" + title + "\": " + year + " year, " + pages + " pages.";
+    }
+
+}
