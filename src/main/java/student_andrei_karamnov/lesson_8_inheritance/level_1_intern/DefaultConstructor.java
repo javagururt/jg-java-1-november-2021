5d1c7890278719f53ec099c120dbac8c1411b591
Andrei Karamnov <unrealandre@gmail.com>
2022-01-23 20:09:30 +0200

    homework lesson 8

---
+++ src/main/java/student_andrei_karamnov/lesson_8_inheritance/level_1_intern/DefaultConstructor.java
@@ -0,0 +1,8 @@
+package student_andrei_karamnov.lesson_8_inheritance.level_1_intern;
+
+class DefaultConstructor {
+
+    public DefaultConstructor() {
+        System.out.println("Create an empty constructor");
+    }
+}
