654d74a89c837766d631d5aa93d4a94c0fe26a98
Dmitrijs-Sinkevics <dmitrijs.sinkevics@gmail.com>
2022-01-24 23:17:32 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/student_dmitrijs_sinkevics/lesson_1/lvl_4/Task11.java
    #	src/main/java/student_dmitrijs_sinkevics/lesson_2/lvl_1/Task1.java
    #	src/main/java/student_dmitrijs_sinkevics/lesson_2/lvl_1/Task2.java
    #	src/main/java/student_dmitrijs_sinkevics/lesson_2/lvl_2/Task8.java
    #	src/main/java/student_dmitrijs_sinkevics/lesson_2/lvl_3/Task10.java
    #	src/main/java/student_dmitrijs_sinkevics/lesson_2/lvl_3/Task11.java
    #	src/main/java/student_dmitrijs_sinkevics/lesson_2/lvl_3/Task9.java
    #	src/main/java/student_dmitrijs_sinkevics/lesson_2/lvl_5/Task18.java
    #	src/main/java/student_dmitrijs_sinkevics/lesson_2/lvl_6/Task23.java

---
+++ src/main/java/student_dmitrijs_sinkevics/lesson_6/lvl_4_junior/ForLoopContinue.java
@@ -0,0 +1,11 @@
+package student_dmitrijs_sinkevics.lesson_6.lvl_4_junior;
+
+public class ForLoopContinue {
+    public static void main(String[] args) {
+        for(int i=10; i>0 ; i--){
+            if(i==5 )
+                continue;
+            System.out.println(i);
+        }
+    }
+}
