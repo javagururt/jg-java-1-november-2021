520e8765b14c30daab9acc8d8c0001e30601f713
JulijaRadchenko <julija_radchenko@inbox.lv>
2022-01-24 22:57:50 +0200

    Lesson_8_homeworks

---
+++ src/main/java/student_julija_radchenko/lesson_8_homeworks/DefaultConstructorV2Demo.java
@@ -0,0 +1,8 @@
+package student_julija_radchenko.lesson_8_homeworks;
+
+public class DefaultConstructorV2Demo {
+    public static void main(String[]args){
+        DefaultConstructorV2 secondConstructor = new DefaultConstructorV2();
+
+    }
+}
