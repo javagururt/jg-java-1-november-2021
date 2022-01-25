35abd2077fd564f27bdaba7a3e4ecc19777b347a
JulijaRadchenko <julija_radchenko@inbox.lv>
2022-01-25 22:14:51 +0200

    Lesson_8_homeworks

---
+++ src/main/java/student_julija_radchenko/lesson_8_homeworks/Porsche911Demo.java
@@ -0,0 +1,8 @@
+package student_julija_radchenko.lesson_8_homeworks;
+
+public class Porsche911Demo {
+    public static void main(String[] args) {
+        Car porsche911 = new Porsche911 (2021, 300);
+
+    }
+}
