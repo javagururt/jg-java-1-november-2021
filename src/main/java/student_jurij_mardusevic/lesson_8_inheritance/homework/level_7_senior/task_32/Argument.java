4a91b00125dea2af00cf28f0839fc0b443677d96
Jurij <jurijmardusevic@gmail.com>
2022-01-26 09:18:27 +0200

    8

---
+++ src/main/java/student_jurij_mardusevic/lesson_8_inheritance/homework/level_7_senior/task_32/Argument.java
@@ -0,0 +1,17 @@
+package student_jurij_mardusevic.lesson_8_inheritance.homework.level_7_senior.task_32;
+
+import teacher.lesson_8_inheritance.homework.level_7_senior.task_32.MathOperation;
+
+class Argument extends MathOperation {
+
+	private double argument;
+
+	public Argument(double argument) {
+		this.argument = argument;
+	}
+
+	@Override
+	public double calculate() {
+		return argument;
+	}
+}
