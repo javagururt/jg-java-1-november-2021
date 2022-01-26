dcf792c8749e23f33d879f46d867964f7e812a29
RolandLV007 <darbameils@inbox.lv>
2022-01-26 23:02:17 +0200

    Merge remote-tracking branch 'origin/master'

--- src/main/java/student_rolands_gudels/lesson_8_inheritance/level_4_junior/Task_18.java
+++ src/main/java/student_rolands_gudels/lesson_8_inheritance/level_4_junior/Task_18.java
@@ -7,5 +7,24 @@ public class Task_18 {
     Реализуйте в нём абстрактные методы из класса
     Shape: calculateArea() и calculatePerimeter().
     Напишите тесты для класса Rectangle.*/
+ protected static class Rectangle extends Task_15.Shape{
+     private double sideOne = 5;
+     private double sideTwo = 4;
 
+     Rectangle(String title) {
+         super(title);
+     }
+
+     @Override
+     double calculateArea() {
+         double rectangleArea = sideOne*sideTwo;
+         return rectangleArea;
+     }
+
+     @Override
+     double calculatePerimeter() {
+         double rectangleAreaPerimeter = (2*sideOne)+(2*sideTwo);
+         return rectangleAreaPerimeter;
+     }
+ }
 }
