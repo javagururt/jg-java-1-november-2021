d2c1e61d7ab2833de23433d013b9f0fcf53ed548
RolandLV007 <darbameils@inbox.lv>
2022-02-01 20:50:27 +0200

    Merge remote-tracking branch 'origin/master'

--- src/main/java/student_rolands_gudels/lesson_9_interfaces/level_2_intern/Task_4.java
+++ src/main/java/student_rolands_gudels/lesson_9_interfaces/level_2_intern/Task_4.java
@@ -5,33 +5,64 @@ public class Task_4 {
 
     Одну и ту же задачу можно решить разными способами.
 
-    Дан класс:
+    Дан класс: */
 
-    class DayOfTheWeekDetectorIfVersion {
+    class DayOfTheWeekDetectorIfVersion implements DayOfTheWeekDetector, DetectDayNameArra {
 
-        // Try to use switch here!!
         public String detectDayName(int number) {
-            if (number == 1) {
-                return "Monday";
-            } else if (number == 2) {
-                return "Tuesday";
-            } else if (number == 3) {
-                return "Wednesday";
-            } else if (number == 4) {
-                return "Thursday";
-            } else if (number == 5) {
-                return "Friday";
-            } else if (number == 6) {
-                return "Saturday";
-            } else if (number == 7) {
-                return "Sunday";
-            } else {
-                return "Please input a valid number between 1 and 7";
+             if (number == 1) {
+                 return "Monday";
+             } else if (number == 2) {
+                 return "Tuesday";
+             } else if (number == 3) {
+                 return "Wednesday";
+             } else if (number == 4) {
+                 return "Thursday";
+             } else if (number == 5) {
+                 return "Friday";
+             } else if (number == 6) {
+                 return "Saturday";
+             } else if (number == 7) {
+                 return "Sunday";
+             } else {
+                 return "Please input a valid number between 1 and 7";
+             }
+         }
+         /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        // Try to use switch here!!
+       @Override public String detectDayNameSwit(int number) {
+            switch (number) {
+                case 1:
+                    return ("Monday");
+                case 2:
+                    return("Tuesday");
+                case 3:
+                    return("Wednesday");
+                case 4:
+                    return("Thursday");
+                case 5:
+                    return("Friday");
+                case 6:
+                    return("Saturday");
+                case 7:
+                    return("Sunday");
+                default:
+                    return ("Please input a valid number between 1 and 7");
             }
         }
-    }
 
-    Создайте для этого класса интерфейс DayOfTheWeekDetector.
+        @Override public String detectDayNameArra(int number) {
+            String[] numbers = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+            for (int i = 0; i < numbers.length; i++){
+                return numbers[number];}
+            return numbers[number];
+        }
+           // return null;
+
+    }
+}
+
+   /* Создайте для этого класса интерфейс DayOfTheWeekDetector.
     Сделайте так, чтобы класс DayOfTheWeekDetectorIfVersion реализовывал
     интерфейс DayOfTheWeekDetector.
 
@@ -42,4 +73,4 @@ public class Task_4 {
     дня недели.
 
     Протестируйте все имплементации.*/
-}
+
