eecbe78d05401d54354c835b53ed91ce71c417f6
RolandLV007 <darbameils@inbox.lv>
2022-02-02 19:58:12 +0200

    Merge remote-tracking branch 'origin/master'

--- src/main/java/student_rolands_gudels/lesson_9_interfaces/level_2_intern/DetectorTest.java
+++ src/main/java/student_rolands_gudels/lesson_9_interfaces/level_2_intern/DetectorTest.java
@@ -1,18 +1,24 @@
 package student_rolands_gudels.lesson_9_interfaces.level_2_intern;
 
-public class DetectorTest   {
-    public static void main(String[] args) {
-    DetectorTest detectorTest = new DetectorTest();
-    detectorTest.ifTest();
-}
+import java.util.Objects;
 
-     public void ifTest() {
+public class DetectorTest {
+    public static void main(String[] args) {
+        DetectorTest detectorTest = new DetectorTest();
+        detectorTest.ifTest();
+    }
+
+    public void ifTest() {
         Task_4.DayOfTheWeekDetectorIfVersion dayOfTheWeekDetectorIfVersion = new Task_4.DayOfTheWeekDetectorIfVersion();
-            String actual = dayOfTheWeekDetectorIfVersion.detectDayName(2);
-            String expected = "Tuesday";
-            if (actual==expected) {System.out.println("Passed");}
+        String actual = dayOfTheWeekDetectorIfVersion.detectDayName(2);
+        String expected = "Tuesday";
+        if (Objects.equals(actual, expected)) {
+            System.out.println("If test Passed");
+        } else {
+            System.out.println("Tf test Failed + actual was " + actual + " but expected was " + expected);
         }
     }
+}
 
 
 
