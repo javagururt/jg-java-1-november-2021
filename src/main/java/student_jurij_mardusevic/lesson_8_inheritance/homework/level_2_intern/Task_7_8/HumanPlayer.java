6a9ce0c1d137642ab20888fd3ecde5be308b1265
Jurij <jurijmardusevic@gmail.com>
2022-02-02 15:37:15 +0200

    8

---
+++ src/main/java/student_jurij_mardusevic/lesson_8_inheritance/homework/level_2_intern/Task_7_8/HumanPlayer.java
@@ -0,0 +1,9 @@
+package student_jurij_mardusevic.lesson_8_inheritance.homework.level_2_intern.Task_7_8;
+
+class HumanPlayer extends Player{
+
+    public HumanPlayer(String name) {
+        super(name);
+    }
+
+}
