efc04a43b55ac1a9ebad4c7baca4dc1e5464a80f
RolandLV007 <darbameils@inbox.lv>
2022-02-02 19:47:44 +0200

    Merge remote-tracking branch 'origin/master'

--- src/main/java/student_rolands_gudels/lesson_9_interfaces/level_2_intern/DayOfTheWeekDetector.java
+++ src/main/java/student_rolands_gudels/lesson_9_interfaces/level_2_intern/DayOfTheWeekDetector.java
@@ -1,6 +1,6 @@
 package student_rolands_gudels.lesson_9_interfaces.level_2_intern;
 
-public interface DayOfTheWeekDetector {
-    public String detectDayNameSwit(int number);
+ interface DayOfTheWeekDetector {
+     String detectDayNameSwit(int number);
 }
 
--- src/main/java/student_rolands_gudels/lesson_9_interfaces/level_2_intern/DetectorTest.java
+++ src/main/java/student_rolands_gudels/lesson_9_interfaces/level_2_intern/DetectorTest.java
@@ -1,17 +1,18 @@
 package student_rolands_gudels.lesson_9_interfaces.level_2_intern;
 
-public class DetectorTest  {
+public class DetectorTest   {
+    public static void main(String[] args) {
+    DetectorTest detectorTest = new DetectorTest();
+    detectorTest.ifTest();
+}
 
-
-  /*  void ifTest() {
-        Task_4.DayOfTheWeekDetectorIfVersion dayOfTheWeekDetectorIfVersion = new DayOfTheWeekDetector() {
+     public void ifTest() {
+        Task_4.DayOfTheWeekDetectorIfVersion dayOfTheWeekDetectorIfVersion = new Task_4.DayOfTheWeekDetectorIfVersion();
             String actual = dayOfTheWeekDetectorIfVersion.detectDayName(2);
             String expected = "Tuesday";
-        if(actual ==expected)
-            {
-                System.out.println("Passed");
-            }
-        }*/
-}
+            if (actual==expected) {System.out.println("Passed");}
+        }
+    }
+
 
 
