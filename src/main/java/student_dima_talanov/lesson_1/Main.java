63b45221674c5d0bec995995b6e7afeabdbafd77
DmitrijT <di_riga@inbox.lv>
2022-02-03 20:13:14 +0200

    :)

---
+++ src/main/java/student_dima_talanov/lesson_1/Main.java
@@ -0,0 +1,15 @@
+package student_dima_talanov.lesson_1;
+
+public class Main {
+
+        public static void main(String[] args) {
+            int number1 = 100;
+            int number2 = 200;
+            int number3 = 300;
+            System.out.println(number1);
+            System.out.println(number2);
+            System.out.println(number3);
+            System.out.println("100 200 300");
+        }
+    }
+
---
+++ src/main/java/student_dima_talanov/lesson_1/MySolution.java
@@ -0,0 +1,11 @@
+package student_dima_talanov.lesson_1;
+import java.util.Scanner;
+public class MySolution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        System.out.println("Температура воздуха сегодня:" + t + "градусов.");
+    }
+}
+
