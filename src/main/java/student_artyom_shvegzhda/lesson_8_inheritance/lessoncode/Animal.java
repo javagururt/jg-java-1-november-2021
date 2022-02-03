13c7525200de88c48859f2a9a686a0491e99ac80
Ashvegzhda <ashvegzhda@icloud.com>
2022-02-03 09:14:44 +0200

    All your bases are now belong to us

--- src/main/java/student_artyom_shvegzhda/lesson_8_inheritance/lessoncode/Animal.java
+++
@@ -1,6 +0,0 @@
-package student_artyom_shvegzhda.lesson_8_inheritance.lessoncode;
-
-abstract class Animal {
-
-    abstract void breath();
-}
--- src/main/java/student_artyom_shvegzhda/lesson_9_interfaces/lessoncode/Dancer.java
+++
@@ -1,6 +0,0 @@
-package student_artyom_shvegzhda.lesson_9_interfaces.lessoncode;
-
-public interface Dancer {
-
-    void dance();
-}
