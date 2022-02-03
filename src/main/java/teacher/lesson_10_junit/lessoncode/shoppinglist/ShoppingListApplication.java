a29db1bba0b980d2fff14b46b06fe4c06cec3692
Ruslans Tufetulovs <ruslans.tufetulovs@gmail.com>
2022-02-03 20:39:00 +0200

    lesson 10 junit

--- src/main/java/teacher/lesson_10_junit/lessoncode/shoppinglist/ShoppingListApplication.java
+++ src/main/java/teacher/lesson_10_junit/lessoncode/shoppinglist/ShoppingListApplication.java
@@ -1,6 +1,7 @@
 package teacher.lesson_10_junit.lessoncode.shoppinglist;
 
 import teacher.lesson_10_junit.lessoncode.shoppinglist.businesslogic.ProductService;
+import teacher.lesson_10_junit.lessoncode.shoppinglist.businesslogic.ValidationService;
 import teacher.lesson_10_junit.lessoncode.shoppinglist.database.ProductRepository;
 import teacher.lesson_10_junit.lessoncode.shoppinglist.database.ProductRepositoryArrayImpl;
 import teacher.lesson_10_junit.lessoncode.shoppinglist.ui.AddProduct;
@@ -13,6 +14,7 @@ class ShoppingListApplication {
 
     public static void main(String[] args) {
         ProductRepository productRepository = new ProductRepositoryArrayImpl();
+        ValidationService validationService = new ValidationService();
 
         ProductService service = new ProductService(productRepository, validationService);
 
