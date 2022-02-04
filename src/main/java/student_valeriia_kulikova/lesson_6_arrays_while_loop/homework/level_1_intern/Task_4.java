058c80dda7b3185913c3dbb1f81033b9db306b32
ValeriaKul <lera.dribnyuk@mail.ru>
2022-02-04 22:36:03 +0200

    ;)

---
+++ src/main/java/student_valeriia_kulikova/lesson_6_arrays_while_loop/homework/level_1_intern/Task_4.java
@@ -0,0 +1,14 @@
+package student_valeriia_kulikova.lesson_6_arrays_while_loop.homework.level_1_intern;
+
+public class Task_4 {
+    static class WhileDefinition4 {
+
+        public static void main(String[] args) {
+            int i = 1;
+            while (i <= 100) {
+                System.out.println(i);
+                i++;
+            }
+        }
+    }
+}
