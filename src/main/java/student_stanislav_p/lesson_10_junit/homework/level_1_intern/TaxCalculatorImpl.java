3a6f231f0888b4a62d2a54271b65e3ad4f2969ea
StanislavP <Perminovsb@mail.ee>
2022-02-08 01:44:22 +0200

    Lesson 10

---
+++ src/main/java/student_stanislav_p/lesson_10_junit/homework/level_1_intern/TaxCalculatorImpl.java
@@ -0,0 +1,13 @@
+package student_stanislav_p.lesson_10_junit.homework.level_1_intern;
+
+import java.math.BigDecimal;
+
+public class TaxCalculatorImpl implements TaxCalculator {
+
+    @Override
+    public BigDecimal calculateTax (BigDecimal income) {
+
+
+        return null;
+    };
+}
