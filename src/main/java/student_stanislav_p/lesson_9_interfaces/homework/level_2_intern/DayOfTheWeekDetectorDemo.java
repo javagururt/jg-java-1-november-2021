3c221ce381d93c2523aa3795918566337657449e
StanislavP <Perminovsb@mail.ee>
2022-02-07 18:13:21 +0200

    Lesson 9

--- src/main/java/student_stanislav_p/lesson_9_interfaces/homework/level_2_intern/DayOfTheWeekDetectorDemo.java
+++ src/main/java/student_stanislav_p/lesson_9_interfaces/homework/level_2_intern/DayOfTheWeekDetectorDemo.java
@@ -7,7 +7,6 @@ public class DayOfTheWeekDetectorDemo {
     DayOfTheWeekDetector typeOfInterface;
 
 
-
     public DayOfTheWeekDetectorDemo(DayOfTheWeekDetector typeOfInterface) {
 
         this.typeOfInterface = typeOfInterface;
@@ -29,11 +28,7 @@ public class DayOfTheWeekDetectorDemo {
 
         typeofImplementation = new DayOfTheWeekDetectorDemo(new DayOfTheWeekDetectorSwitchVersion());
 
-        typeofImplementation.run( );
-
-
-
-
+        typeofImplementation.run();
 
 
     }
@@ -41,48 +36,37 @@ public class DayOfTheWeekDetectorDemo {
     public void run() {
 
 
-        System.out.println("Demo for "+nameOfImplementation() + " method of implementation.");
+        System.out.println("Demo for " + nameOfImplementation() + " method of implementation.");
 
         int dayNumber = getDayNumberFromUser();
 
-        System.out.println("Your chose -> "+ nameOfTheDay(dayNumber));
+        System.out.println("Your chose -> " + nameOfTheDay(dayNumber));
 
         System.out.println("===========================================");
 
 
     }
 
-    public String nameOfTheDay (int dayNumber){
+    public String nameOfTheDay(int dayNumber) {
         return typeOfInterface.detectDayName(dayNumber);
 
     }
 
-    public String nameOfImplementation(){
+    public String nameOfImplementation() {
 
         String nameOfImplement = typeOfInterface.toString();
 
-        int pos1 = nameOfImplement.indexOf("n.")+2;
+        int pos1 = nameOfImplement.indexOf("n.") + 2;
 
         int pos2 = nameOfImplement.indexOf("@");
 
-        return  nameOfImplement.substring(pos1, pos2);
+        return nameOfImplement.substring(pos1, pos2);
     }
 
-   private int getDayNumberFromUser() {
+    private int getDayNumberFromUser() {
 
         Scanner scanner = new Scanner(System.in);
         System.out.print("Input day number between 1 and 7 : ");
         return scanner.nextInt();
     }
-
-
-    private void checkTestResult(boolean condition, String testName) {
-        if (condition) {
-            System.out.println(testName + " = OK!");
-        } else {
-            System.out.println(testName + " = FAIL!");
-        }
-    }
-
-
 }
