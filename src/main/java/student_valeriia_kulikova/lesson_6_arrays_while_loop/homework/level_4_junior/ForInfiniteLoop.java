c89df87681b903e5be16c4687a19db251e979620
ValeriaKul <lera.dribnyuk@mail.ru>
2022-02-07 22:45:58 +0200

    lesson 6

---
+++ src/main/java/student_valeriia_kulikova/lesson_6_arrays_while_loop/homework/level_4_junior/ForInfiniteLoop.java
@@ -0,0 +1,10 @@
+package student_valeriia_kulikova.lesson_6_arrays_while_loop.homework.level_4_junior;
+
+public class ForInfiniteLoop {
+    public static void main(String[] args) {
+
+        for (int i = 1; i > 0; i++ ) {
+
+        }
+    }
+}
