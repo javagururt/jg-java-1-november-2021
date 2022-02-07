ace8dc74946524af517381f32b4d0fa78685bb0b
Dmitrijs-Sinkevics <dmitrijs.sinkevics@gmail.com>
2022-02-08 00:22:40 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/student_dmitrijs_sinkevics/lesson_1/lvl_4/Task11.java
    #	src/main/java/student_dmitrijs_sinkevics/lesson_2/lvl_1/Task1.java
    #	src/main/java/student_dmitrijs_sinkevics/lesson_2/lvl_1/Task2.java
    #	src/main/java/student_dmitrijs_sinkevics/lesson_2/lvl_2/Task8.java
    #	src/main/java/student_dmitrijs_sinkevics/lesson_2/lvl_3/Task10.java
    #	src/main/java/student_dmitrijs_sinkevics/lesson_2/lvl_3/Task11.java
    #	src/main/java/student_dmitrijs_sinkevics/lesson_2/lvl_3/Task9.java
    #	src/main/java/student_dmitrijs_sinkevics/lesson_2/lvl_5/Task18.java
    #	src/main/java/student_dmitrijs_sinkevics/lesson_2/lvl_6/Task23.java

---
+++ src/main/java/student_dmitrijs_sinkevics/lesson_8/lvl_1_intern/DefaultConstructorV2.java
@@ -0,0 +1,7 @@
+package student_dmitrijs_sinkevics.lesson_8.lvl_1_intern;
+
+public class DefaultConstructorV2 {
+    DefaultConstructorV2(){
+        System.out.println("constructor works");
+    }
+}
---
+++ src/main/java/student_dmitrijs_sinkevics/lesson_8/lvl_1_intern/DefaultConstructorV3Demo.java
@@ -0,0 +1,16 @@
+package student_dmitrijs_sinkevics.lesson_8.lvl_1_intern;
+
+public class DefaultConstructorV3Demo {
+    public static void main(String[] args) {
+        DefaultConstructorV3 example = new DefaultConstructorV3();
+        String exampleFullName=example.getFullName();
+        int exampleAge=example.getAge();
+        boolean exampleMale=example.isMale();
+        boolean exampleFemale=example.isFemale();
+
+        System.out.println("name: "+exampleFullName);
+        System.out.println("age: "+exampleAge);
+        System.out.println("isMale: "+exampleMale);
+        System.out.println("isFemale: "+exampleFemale);
+    }
+}
