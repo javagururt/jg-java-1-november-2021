ac6a684bee836c4be38b17a6e174170c44e5047a
hq4proxd <salamotkapitanov@gmail.com>
2022-02-12 12:42:23 +0200

    -_-

---
+++ src/main/java/student_victor_veselov/lesson_11/Task6/BookApplication.java
@@ -0,0 +1,22 @@
+package student_victor_veselov.lesson_11.Task6;
+
+public class BookApplication {
+    public static void main(String[] args) {
+        BookDatabaseImpl bookDatabase = new BookDatabaseImpl();
+        BookService service = new BookService(bookDatabase);
+
+        Book bookLee  = new Book("Bruce Lee","Dragon");
+        Book bookLee1  = new Book("Bruce Lee","Dragon1");
+        Book bookLee2  = new Book("Bruce Lee","Dragon1");
+        service.saveBook(bookLee);
+        service.saveBook(bookLee1);
+        service.saveBook(bookLee2);
+
+        System.out.println(bookDatabase.database.get(0));
+        System.out.println(bookDatabase.database.get(1));
+        System.out.println(bookDatabase.database.get(2));
+
+
+
+    }
+}
