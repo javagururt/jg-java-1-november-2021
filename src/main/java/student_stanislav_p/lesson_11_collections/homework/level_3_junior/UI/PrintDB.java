994ab1c8ef7ffef6cf44eea36eed015d7bb00a20
StanislavP <Perminovsb@mail.ee>
2022-02-13 19:44:39 +0200

    Lesson 11

--- src/main/java/student_stanislav_p/lesson_11_collections/homework/level_3_junior/UI/PrintDB.java
+++ src/main/java/student_stanislav_p/lesson_11_collections/homework/level_3_junior/UI/PrintDB.java
@@ -1,5 +1,6 @@
 package student_stanislav_p.lesson_11_collections.homework.level_3_junior.UI;
 
+import student_stanislav_p.lesson_11_collections.homework.level_3_junior.Repository.Book;
 import student_stanislav_p.lesson_11_collections.homework.level_3_junior.Service.BookDatabaseImpl;
 
 public class PrintDB {
@@ -18,6 +19,12 @@ public class PrintDB {
         System.out.println("--------------------------------------------------");
         System.out.println("Numbers of records : " + bookDatabase.database.size() + " records.");
         System.out.println("--------------------------------------------------");
+
+    }
+
+    public void printBook(Book book){
+
+       System.out.println("Find book by ID :"+ book.getId() + " Author is - "+book.getAuthor()+ " Title is - "+book.getTitle());
 
     }
 }
