3f4641d4898cef42079b4367c500b6ca86910273
JulijaRadchenko <julija_radchenko@inbox.lv>
2022-02-14 21:51:12 +0200

    lib

---
+++ src/main/java/student_julija_radchenko/lesson_10_homeworks/TaxCalculator.java
@@ -0,0 +1,10 @@
+package student_julija_radchenko.lesson_10_homeworks;
+
+import java.math.BigDecimal;
+
+public interface TaxCalculator {
+
+
+        BigDecimal calculateTax(BigDecimal income);
+
+    }
