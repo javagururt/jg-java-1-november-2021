1be016182d5860daa23d91182b75c46f49b6e512
mihailsnikolajevs <mike2309@inbox.lv>
2022-02-15 20:42:55 +0200

    lesson3_level4_task15_junior_phone_

---
+++ src/main/java/student_mihails_nikolajevs/lesson_3/level_4_junior_/Phone.java
@@ -0,0 +1,49 @@
+package student_mihails_nikolajevs.lesson_3.level_4_junior_;
+/*
+Найти ошибку в коде.
+Исправить и запустить программу.
+Менять можно только класс Phone.
+
+class Phone {
+
+    String model;{
+
+    Book(String newModel) {
+        this.model = newModel;
+    }
+
+    String getModel() {
+        return this.model;
+    }
+
+}
+
+class PhoneDemo {
+
+    public static void main(String[] args) {
+        Phone myPhone = new Phone("Huawei");
+        String phoneModel = myPhone.getModel();
+        System.out.println("Phone model = " + phoneModel);
+    }
+
+}
+
+ */
+
+public class Phone {
+
+
+        String model;
+
+
+
+             Phone(String phoneModel) {
+            this.model = phoneModel;
+        }
+
+            String getModel () {
+            return this.model;
+        }
+
+
+    }
---
+++ src/main/java/student_mihails_nikolajevs/lesson_3/level_4_junior_/PhoneDemo.java
@@ -0,0 +1,11 @@
+package student_mihails_nikolajevs.lesson_3.level_4_junior_;
+
+public class PhoneDemo {
+
+    public static void main(String[] args) {
+        Phone myPhone = new Phone("Huawei");
+        String phoneModel = myPhone.getModel();
+        System.out.println("Phone model = " + phoneModel);
+    }
+
+}
