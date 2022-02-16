aae07528015a2fd1fd1356903993fe65688ad9df
mihailsnikolajevs <mike2309@inbox.lv>
2022-02-16 19:09:24 +0200

    lesson3_level5_task20_middle_dog_

---
+++ src/main/java/student_mihails_nikolajevs/lesson_3/level_5_middle/Dog_3.java
@@ -0,0 +1,50 @@
+package student_mihails_nikolajevs.lesson_3.level_5_middle;
+/*
+Тема: создание класса Dog. Продолжение.
+
+Создать класс собака (Dog)
+и продемонстрировать его работу с ним в программе.
+
+Бизнес требования:
+- Должна быть возможность увеличивать возраст собаки на один.
+
+PS: научите собаку праздновать свой день рождения,
+создайте в классе Dog метод:
+
+    void happyBirthday() {
+        // напишите тут команду увеличения возраста на 1
+    }
+
+Создать отдельный класс DogDemo,
+объявить в нём main() метод и продемонстрировать
+использование класса собака (Dog).
+
+Создайте собаку,
+заставьте её подать голос,
+заставте её отпраздновать день рождение,
+заставьте её подать голос снова и убедитесь в том,
+что её возраст увеличился на 1.
+ */
+public class Dog_3 {
+
+    int age = 3;
+    int ageIncrease = 0;
+    int ageNow = age;
+
+    public Dog_3(){
+    }
+
+    void voice(){
+        System.out.println("Dog Age " + ageNow);
+        System.out.println("Woof!!! Woof!!!");
+
+        ageIncrease++;
+        ageNow = age + ageIncrease;
+    }
+
+    void happyBirthday() {
+     System.out.println("Happy Birthday, Teddy !!!");
+     System.out.println("Today's Age are " + ageNow + " !!!");
+    }
+
+}
---
+++ src/main/java/student_mihails_nikolajevs/lesson_3/level_5_middle/Dog_3Demo.java
@@ -0,0 +1,12 @@
+package student_mihails_nikolajevs.lesson_3.level_5_middle;
+
+public class Dog_3Demo {
+
+    public static void main(String[] args) {
+        Dog_3 myDog = new Dog_3();
+        myDog.voice();
+        System.out.println("Woof!!! Woof!!! Woof!!!");
+        myDog.happyBirthday();
+    }
+
+}
