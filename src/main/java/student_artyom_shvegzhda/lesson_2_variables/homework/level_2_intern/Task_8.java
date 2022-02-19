ae437a205f5ebb21f7dabffe98af43dd887cc847
Ashvegzhda <ashvegzhda@icloud.com>
2022-02-19 17:52:05 +0200

    All your bases are now belong to us

---
+++ src/main/java/student_artyom_shvegzhda/lesson_2_variables/homework/level_2_intern/Task_8.java
@@ -0,0 +1,14 @@
+package student_artyom_shvegzhda.lesson_2_variables.homework.level_2_intern;
+
+import java.util.Scanner;
+
+public class Task_8 {
+    public static void main(String[] args) {
+        System.out.println("Enter your name:");
+        Scanner scanner = new Scanner(System.in);
+
+        String userName = scanner.next();
+        System.out.println("Hello, your name is " + userName);
+
+    }
+}
