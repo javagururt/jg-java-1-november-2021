9d650c3a6598c0d56c1f7d680dffdd5530b5b630
Ashvegzhda <ashvegzhda@icloud.com>
2022-02-19 17:52:05 +0200

    All your bases are now belong to us

---
+++ src/main/java/student_artyom_shvegzhda/lesson_2_variables/homework/level_2_intern/LoginService.java
@@ -0,0 +1,10 @@
+package student_artyom_shvegzhda.lesson_2_variables.homework.level_2_intern;
+
+class LoginService {
+
+    public static void main(String[] args) {
+        System.out.println("You can login!");
+    }
+
+
+}
