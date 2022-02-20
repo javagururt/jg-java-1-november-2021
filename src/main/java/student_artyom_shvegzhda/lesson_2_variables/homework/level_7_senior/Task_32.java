b853420ff0386cd1ccc77da5743539fc1a06c943
Ashvegzhda <ashvegzhda@icloud.com>
2022-02-20 16:51:27 +0200

    Merge remote-tracking branch 'origin/master'

---
+++ src/main/java/student_artyom_shvegzhda/lesson_2_variables/homework/level_7_senior/Task_32.java
@@ -0,0 +1,14 @@
+package student_artyom_shvegzhda.lesson_2_variables.homework.level_7_senior;
+
+public class Task_32 {
+    public static void main(String[] args) {
+        /*
+        Многострочный комментарий
+        Да да :-)
+         */
+
+    //Однострочный
+
+    }
+
+}
