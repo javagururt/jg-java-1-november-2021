911cd72b73c6ab3b92d60874f7ba730efa3095f9
Ashvegzhda <ashvegzhda@icloud.com>
2022-02-20 16:38:02 +0200

    Merge remote-tracking branch 'origin/master'

---
+++ src/main/java/student_artyom_shvegzhda/lesson_2_variables/homework/level_6_middle/AttentionPlease.java
@@ -0,0 +1,10 @@
+package student_artyom_shvegzhda.lesson_2_variables.homework.level_6_middle;
+
+class AttentionPlease {
+
+
+
+    public static void main(String[] args) {
+        System.out.println("Code outside the method will not compile!");
+    }
+}
