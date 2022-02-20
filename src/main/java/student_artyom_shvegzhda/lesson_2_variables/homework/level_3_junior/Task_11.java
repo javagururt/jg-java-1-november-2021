ce1c79de3e2db78b2183ade2a0469677790c2de2
Ashvegzhda <ashvegzhda@icloud.com>
2022-02-20 16:05:24 +0200

    All your bases are now belong to us

---
+++ src/main/java/student_artyom_shvegzhda/lesson_2_variables/homework/level_3_junior/Task_11.java
@@ -0,0 +1,18 @@
+package student_artyom_shvegzhda.lesson_2_variables.homework.level_3_junior;
+
+import java.util.Scanner;
+
+public class Task_11 {
+    public static void main(String[] args) {
+        System.out.println("Enter three numbers to get average out of them:");
+        Scanner scanner = new Scanner(System.in);
+
+        double firstOne = scanner.nextDouble();
+        double secondOne = scanner.nextDouble();
+        double thirdOne = scanner.nextDouble();
+
+        double averageOutOfThree = (firstOne + secondOne + thirdOne) / 3;
+
+        System.out.println("Average of three numbers is: " + averageOutOfThree);
+    }
+}
