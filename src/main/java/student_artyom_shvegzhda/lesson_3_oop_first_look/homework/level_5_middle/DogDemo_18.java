bdcc654388eb468e2ac6b5b9b9332af94be982c3
Ashvegzhda <ashvegzhda@icloud.com>
2022-02-24 16:54:43 +0200

    hw

---
+++ src/main/java/student_artyom_shvegzhda/lesson_3_oop_first_look/homework/level_5_middle/DogDemo_18.java
@@ -0,0 +1,11 @@
+package student_artyom_shvegzhda.lesson_3_oop_first_look.homework.level_5_middle;
+
+public class DogDemo_18 {
+    public static void main(String[] args) {
+    Dog_18 myDog = new Dog_18("Ruslan");
+        System.out.println(myDog.voice());
+        System.out.println(myDog.voice());
+        System.out.println(myDog.voice());
+    }
+
+}
