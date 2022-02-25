be7716d60c46217afa936d6005db7ff6b15d6ff1
StanislavP <Perminovsb@mail.ee>
2022-02-25 20:06:50 +0200

    Lesson 12

--- src/main/java/student_stanislav_p/lesson_12_exceptions/homework/level_1_intern/ExceptionDemo.java
+++ src/main/java/student_stanislav_p/lesson_12_exceptions/homework/level_1_intern/ExceptionDemo.java
@@ -3,12 +3,20 @@ package student_stanislav_p.lesson_12_exceptions.homework.level_1_intern;
 public class ExceptionDemo {
 
     public static void main(String[] args) {
-        MyExceptionClass mc = new MyExceptionClass();
+        MyExceptionClass mc = new MyExceptionClass("Message 1",1);
         String str = mc.getMessage();
         System.out.println("str = " + str);
     }
 }
-class MyExceptionClass extends Exception
-{
 
+class MyExceptionClass extends Exception{
+
+    private int number;
+    public int getNumber(){return number;}
+    public MyExceptionClass(String message, int num){
+
+        super(message);
+        number=num;
+    }
 }
+
