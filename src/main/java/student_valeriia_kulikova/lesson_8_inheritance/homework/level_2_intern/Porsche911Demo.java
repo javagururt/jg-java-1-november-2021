bd6739fab4a54c64fefd8a6e0682ae103656abcf
ValeriaKul <lera.dribnyuk@mail.ru>
2022-02-25 18:13:25 +0200

    lesson 8 level 2

---
+++ src/main/java/student_valeriia_kulikova/lesson_8_inheritance/homework/level_2_intern/Porsche911Demo.java
@@ -0,0 +1,8 @@
+package student_valeriia_kulikova.lesson_8_inheritance.homework.level_2_intern;
+
+public class Porsche911Demo {
+    public static void main(String[] args) {
+        Porshe911 car = new Porshe911(2021,300);
+
+    }
+}
