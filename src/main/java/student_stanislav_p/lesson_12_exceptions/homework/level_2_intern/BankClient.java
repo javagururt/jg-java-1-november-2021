04925b7291619fb2bac1261ad5157c3a2f303395
StanislavP <Perminovsb@mail.ee>
2022-02-28 17:21:00 +0200

    Lesson 12

---
+++ src/main/java/student_stanislav_p/lesson_12_exceptions/homework/level_2_intern/BankClient.java
@@ -0,0 +1,21 @@
+package student_stanislav_p.lesson_12_exceptions.homework.level_2_intern;
+
+    class BankClient {
+
+        private String uid;
+        private String fullName;
+
+        public BankClient(String uid, String fullName) {
+            this.uid = uid;
+            this.fullName = fullName;
+        }
+
+        public String getUid() {
+            return this.uid;
+        }
+
+        public String getFullName() {
+            return this.fullName;
+        }
+
+}
