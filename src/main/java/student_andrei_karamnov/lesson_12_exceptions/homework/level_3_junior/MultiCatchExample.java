478ed8323ad87065861c1a92e3e42c25d762f00f
Andrei Karamnov <unrealandre@gmail.com>
2022-03-01 18:10:55 +0200

    lesson 12

---
+++ src/main/java/student_andrei_karamnov/lesson_12_exceptions/homework/level_3_junior/MultiCatchExample.java
@@ -0,0 +1,24 @@
+package student_andrei_karamnov.lesson_12_exceptions.homework.level_3_junior;
+
+import java.util.Scanner;
+
+class MultiCatchExample {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            System.out.println("Enter a numerator");
+            int a = scanner.nextInt();
+            System.out.println("Enter a denominator");
+            int b = scanner.nextInt();
+            int c = a / b;
+            System.out.println("Integer c is: " + c);
+            System.out.println("Correct!");
+        } catch (ArithmeticException ae) {          //чтоб поймать эту ошибку можно например разделить на 0
+            System.out.println("Arithmetic exception " + ae);
+        } catch (Exception e) {           //чтоб поймать эту ошибку можно попробовать ввести буквы или символы вместо цифр
+            System.out.println("Exception " + e);
+        }
+    }
+}
+ /*Последовательность блоков catch должна идти от частного к более общему. В противном случае будет ошибка компиляции.*/
