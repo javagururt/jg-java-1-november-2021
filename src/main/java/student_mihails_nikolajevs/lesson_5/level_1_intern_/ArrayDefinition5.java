72d10e53939f61d14210243ad1a9f97175a2363a
mihailsnikolajevs <mike2309@inbox.lv>
2022-03-03 23:04:03 +0200

    lesson5_level1_intern_task5_ArrayDefinition5_

---
+++ src/main/java/student_mihails_nikolajevs/lesson_5/level_1_intern_/ArrayDefinition5.java
@@ -0,0 +1,11 @@
+package student_mihails_nikolajevs.lesson_5.level_1_intern_;
+
+public class ArrayDefinition5 {
+
+    public static void main(String[] args) {
+        int [] numbers = {1, 2, 3, 4};
+    }
+}
+
+
+
