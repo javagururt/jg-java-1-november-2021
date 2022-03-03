dd5df3c7ac839fc31d0613fcb87e0a92745b531d
mihailsnikolajevs <mike2309@inbox.lv>
2022-03-03 22:47:42 +0200

    lesson5_level1_intern_task1_

---
+++ src/main/java/student_mihails_nikolajevs/lesson_5/level_1_intern_/Task_1.java
@@ -0,0 +1,22 @@
+package student_mihails_nikolajevs.lesson_5.level_1_intern_;
+/*
+Найдите ошибку в программе и исправить её:
+
+class ArrayDefinition1 {
+
+    public static void main(String[] args) {
+        int numbers = new int[];
+    }
+
+}
+ */
+public class Task_1 {
+
+    // class ArrayDefinition1 {
+
+    public static void main(String[] args) {
+        int [] numbers = new int[]{};
+
+    }
+
+}
