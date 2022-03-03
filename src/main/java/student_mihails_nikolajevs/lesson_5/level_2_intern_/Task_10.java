3b3f397e59c9772d132673af39f44adbd4607194
mihailsnikolajevs <mike2309@inbox.lv>
2022-03-03 23:23:28 +0200

    lesson5_level1_intern_task10_

---
+++ src/main/java/student_mihails_nikolajevs/lesson_5/level_2_intern_/Task_10.java
@@ -0,0 +1,18 @@
+package student_mihails_nikolajevs.lesson_5.level_2_intern_;
+/*
+Напишите программу, в которой:
+ - создайте массив целых чисел длиной 3,
+ - заполните каждую ячейку массива любым целым числом,
+ - выведите значение каждой ячейки на консоль.
+ */
+
+import java.util.Arrays;
+
+public class Task_10 {
+
+    public static void main(String[] args) {
+        int[] numbers = {2, 4, 6};
+
+        System.out.println("Array: " + Arrays.toString(numbers));
+    }
+}
