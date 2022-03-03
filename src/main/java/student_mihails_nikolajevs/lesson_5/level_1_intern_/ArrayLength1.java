3f7b603baeca32658399a89724abcd85f0d1ee75
mihailsnikolajevs <mike2309@inbox.lv>
2022-03-03 23:09:46 +0200

    lesson5_level1_intern_task7_ArrayLength1_

---
+++ src/main/java/student_mihails_nikolajevs/lesson_5/level_1_intern_/ArrayLength1.java
@@ -0,0 +1,12 @@
+package student_mihails_nikolajevs.lesson_5.level_1_intern_;
+
+public class ArrayLength1 {
+
+    public static void main(String[] args) {
+        int[] numbers = new int[2];
+
+        System.out.println(numbers[0]);
+    }
+
+
+}
