1705677180c83dfcb80eeea53fc39fa03d05647b
mihailsnikolajevs <mike2309@inbox.lv>
2022-03-04 22:30:11 +0200

    lesson6_level4_junior_whileInfiniteLoop

---
+++ src/main/java/student_mihails_nikolajevs/lesson_6/level_4_junior_/WhileInfiniteLoop.java
@@ -0,0 +1,19 @@
+package student_mihails_nikolajevs.lesson_6.level_4_junior_;
+/*
+Создайте класс WhileInfiniteLoop и в нём напишите бесконечный
+цикл при помощи цикла while.
+
+Создайте класс ForInfiniteLoop и в нём напишите бесконечный
+цикл при помощи цикла for.
+ */
+
+public class WhileInfiniteLoop {
+
+    public static void main(String[] args) {
+
+        while (true) {
+
+            System.out.println(" INFINITY ");
+        }
+    }
+}
