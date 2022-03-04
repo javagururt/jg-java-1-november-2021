e225826722e845f6b55005aec8ac1280c67c328f
ValeriaKul <lera.dribnyuk@mail.ru>
2022-03-04 17:30:21 +0200

    lesson 9 level 2

---
+++ src/main/java/student_valeriia_kulikova/lesson_9_interfaces/homework/level_2_intern/DayOfTheWeekDetectorDemo.java
@@ -0,0 +1,30 @@
+package student_valeriia_kulikova.lesson_9_interfaces.homework.level_2_intern;
+
+import java.util.Scanner;
+
+public class DayOfTheWeekDetectorDemo {
+
+    DayOfTheWeekDetector type;
+
+    public DayOfTheWeekDetectorDemo (DayOfTheWeekDetector type) {
+        this.type = type;
+    }
+
+    public static void main(String[] args) {
+    DayOfTheWeekDetectorDemo type2;
+    type2 = new DayOfTheWeekDetectorDemo(new DayOfTheWeekDetectorIfVersion());
+    type2.run();
+    type2 = new DayOfTheWeekDetectorDemo(new DayOfTheWeekDetectorSwitch());
+    type2.run();
+    type2 = new DayOfTheWeekDetectorDemo(new DayOfTheWeekDetectorArray());
+    type2.run();
+    }
+
+    public void run () {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your number: ");
+        int numberOfDay = scanner.nextInt();
+        String dayName = type.detectDayName(numberOfDay);
+        System.out.println("Your day: " + dayName);
+    }
+}
