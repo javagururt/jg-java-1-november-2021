ce68ffbc6b45e404808f26138b7ca5c7328702e0
mihailsnikolajevs <mike2309@inbox.lv>
2022-03-04 11:42:00 +0200

    lesson5_level3_junior_task22_ForLoopDefinition7_

---
+++ src/main/java/student_mihails_nikolajevs/lesson_5/level_3_junior_/ForLoopDefinition7.java
@@ -0,0 +1,12 @@
+package student_mihails_nikolajevs.lesson_5.level_3_junior_;
+
+public class ForLoopDefinition7 {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++)
+
+            System.out.println(i);
+    }
+}
+
