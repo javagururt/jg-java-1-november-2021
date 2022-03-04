a0bcadbb9dc86103351428daa68966771dec46ec
mihailsnikolajevs <mike2309@inbox.lv>
2022-03-04 19:41:23 +0200

    lesson6_level1_intern_task_5_whileDefinition5

---
+++ src/main/java/student_mihails_nikolajevs/lesson_6/level_1_intern/WhileDefinition5.java
@@ -0,0 +1,16 @@
+package student_mihails_nikolajevs.lesson_6.level_1_intern;
+
+public class WhileDefinition5 {
+// Программа должна выводить числа от 1 до 100 на консоль.
+    public static void main(String[] args) {
+
+        int i = 1;
+
+        while (i <= 100) {
+
+            System.out.println(i);
+
+            i++;
+        }
+    }
+}
