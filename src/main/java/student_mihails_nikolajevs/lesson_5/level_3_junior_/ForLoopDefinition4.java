ef704bfc6d1beb6372e07301612259d2e38f326a
mihailsnikolajevs <mike2309@inbox.lv>
2022-03-04 11:34:05 +0200

    lesson5_level3_junior_task19_ForLoopDefinition4_

---
+++ src/main/java/student_mihails_nikolajevs/lesson_5/level_3_junior_/ForLoopDefinition4.java
@@ -0,0 +1,12 @@
+package student_mihails_nikolajevs.lesson_5.level_3_junior_;
+
+public class ForLoopDefinition4 {
+  // программа должна выводить на консоль числа от 0 до 9.
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++) {
+
+            System.out.println(i);
+        }
+    }
+}
