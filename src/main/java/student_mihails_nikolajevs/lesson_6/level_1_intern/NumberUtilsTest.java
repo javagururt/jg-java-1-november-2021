0b2fc8dada9192bad8bf8efd7832d49b189d0f62
mihailsnikolajevs <mike2309@inbox.lv>
2022-03-04 19:56:17 +0200

    lesson6_level1_intern_task_6_numberUtilsTest

---
+++ src/main/java/student_mihails_nikolajevs/lesson_6/level_1_intern/NumberUtilsTest.java
@@ -0,0 +1,23 @@
+package student_mihails_nikolajevs.lesson_6.level_1_intern;
+
+public class NumberUtilsTest {
+
+    public static void main(String[] args) {
+
+        NumberUtilsTest numberUtilsTest = new NumberUtilsTest();
+        numberUtilsTest.isEvenTest();
+    }
+
+    public void isEvenTest() {
+
+        NumberUtils numberUtils = new NumberUtils();
+        boolean check = true;
+
+        if (check == numberUtils.isEven(2)) {
+            System.out.println(" TEST SUCCESS ");
+
+        } else {
+            System.out.println(" TEST FAILED ");
+        }
+    }
+}
