17cd055301a2bf410ffe83ef5ad3d07e19c6ee94
mihailsnikolajevs <mike2309@inbox.lv>
2022-03-06 18:52:51 +0200

    lesson8_level1_intern_task_4_

---
+++ src/main/java/student_mihails_nikolajevs/lesson_8/level_1_intern_/Task_4_.java
@@ -0,0 +1,18 @@
+package student_mihails_nikolajevs.lesson_8.level_1_intern_;
+/*
+Исправьте код программы:
+
+class Person extend Object {
+
+
+}
+
+PS: в этом задании класс Person должен явно наследоваться
+от класса Object, пропишите наследование правильно.
+ */
+public class Task_4_ {
+
+    class Person extends Object {
+
+   }
+}
