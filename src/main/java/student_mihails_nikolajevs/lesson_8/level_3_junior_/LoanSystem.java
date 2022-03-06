1bb5a1d6cf9805e33f9c12a263a4fb4705f8e698
mihailsnikolajevs <mike2309@inbox.lv>
2022-03-06 20:21:36 +0200

    lesson8_level3_junior_loanSystem_

---
+++ src/main/java/student_mihails_nikolajevs/lesson_8/level_3_junior_/LoanSystem.java
@@ -0,0 +1,24 @@
+package student_mihails_nikolajevs.lesson_8.level_3_junior_;
+
+/*
+Исправьте код программы так, что бы он компилировался.
+
+abstracts class LoanSystem {
+
+}
+ */
+/*
+Исправьте код программы так, что бы он компилировался.
+Реализовывать метод canReceiveLoan() нельзя.
+
+public abstract class LoanSystem {
+
+    abstract boolean canReceiveLoan(Human human);
+ */
+abstract class LoanSystem {
+
+     abstract boolean canReceiveLoan(Human human);
+
+    private class Human {
+    }
+}
