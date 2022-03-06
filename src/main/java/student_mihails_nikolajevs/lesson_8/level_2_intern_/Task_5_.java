872c604ef1776774b9dd1913b30ec1d12f57b0ad
mihailsnikolajevs <mike2309@inbox.lv>
2022-03-06 19:01:38 +0200

    lesson8_level2_intern_task_5_

---
+++ src/main/java/student_mihails_nikolajevs/lesson_8/level_2_intern_/Task_5_.java
@@ -0,0 +1,40 @@
+package student_mihails_nikolajevs.lesson_8.level_2_intern_;
+/*
+то такое множественное наследование в программировании?
+
+Разрешено ли множественное наследование в Java?
+
+
+Исправьте код программы:
+
+class Mother {
+
+}
+
+class Father {
+
+}
+
+class Child extends Mother, Father {
+
+}
+
+PS: вносить изменения можно только в объявление класса Child.
+Класс Child должен явно наследоваться от одного или двух классов
+(Mother и Father). Правильных решений у этой задачи два, выбирайте любое.
+ */
+public class Task_5_ {
+
+   class Mother {
+
+   }
+
+   class Father {
+
+   }
+
+   class Child extends Mother {
+
+   }
+
+}
