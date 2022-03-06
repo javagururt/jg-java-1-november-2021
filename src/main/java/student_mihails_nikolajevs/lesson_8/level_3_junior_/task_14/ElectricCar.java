cb2bd8ff89f964db2186efc399c61980b5b2b629
mihailsnikolajevs <mike2309@inbox.lv>
2022-03-06 21:26:47 +0200

    lesson8_level3_junior_task_14_Car_

---
+++ src/main/java/student_mihails_nikolajevs/lesson_8/level_3_junior_/task_14/ElectricCar.java
@@ -0,0 +1,12 @@
+package student_mihails_nikolajevs.lesson_8.level_3_junior_.task_14;
+
+class ElectricCar extends Car {
+
+    private int batteryCharge;
+
+   @Override
+    void accelerate() {
+       System.out.println("ElectricCar or PetrolCar?");
+   }
+}
+
