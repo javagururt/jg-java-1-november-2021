4d8779ab899ec61983c947cf01c229a2c706a8d0
mihailsnikolajevs <mike2309@inbox.lv>
2022-03-06 13:31:18 +0200

    lesson8_level1_intern_defaultConstructorDemo_

---
+++ src/main/java/student_mihails_nikolajevs/lesson_8/DefaultConstructorDemo.java
@@ -0,0 +1,11 @@
+package student_mihails_nikolajevs.lesson_8;
+
+import java.util.Scanner;
+
+class DefaultConstructorDemo {
+
+    public static void main(String[] args) {
+
+        DefaultConstructor defaultConstructor = new DefaultConstructor();
+    }
+}
