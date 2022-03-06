9bfc0e71c661b081465f623cfef95bee0f771399
mihailsnikolajevs <mike2309@inbox.lv>
2022-03-06 18:21:28 +0200

    lesson8_level1_intern_defaultConstructorV2Demo_

---
+++ src/main/java/student_mihails_nikolajevs/lesson_8/DefaultConstructorV2Demo.java
@@ -0,0 +1,9 @@
+package student_mihails_nikolajevs.lesson_8;
+
+public class DefaultConstructorV2Demo {
+
+    public static void main(String[] args) {
+
+        DefaultConstructorV2 anotherConstructor = new DefaultConstructorV2();
+    }
+}
