565b2c03df1ad3ec5abb65c6d2d7b149fb4b9376
eduards1911 <76965658+eduards1911@users.noreply.github.com>
2022-03-11 20:51:56 +0200

    HW_Eduards_Puzirevskis

--- src/main/java/student_eduards_puzirevskis/lesson_12_exeptions/level_1_intern/task_5/ExceptionDemo.java
+++ src/main/java/student_eduards_puzirevskis/lesson_12_exeptions/level_1_intern/task_5/ExceptionDemo.java
@@ -4,8 +4,8 @@ package student_eduards_puzirevskis.lesson_12_exeptions.level_1_intern.task_5;
 
   public static void main(String[] args) {
 
-   MyExeption myExeption = new MyExeption(1,"My Exception 1");
-   String str = myExeption.getMessage();
-   System.out.println("Exception number: \""+myExeption.getExceptionNumber()+"\". Message: " + str);
+   MyExсeption myExсeption = new MyExсeption(1,"My Exception 1");
+   String str = myExсeption.getMessage();
+   System.out.println("Exception number: \""+ myExсeption.getExceptionNumber()+"\". Message: " + str);
   }
 }
--- src/main/java/student_eduards_puzirevskis/lesson_12_exeptions/level_1_intern/task_5/MyExсeption.java
+++ src/main/java/student_eduards_puzirevskis/lesson_12_exeptions/level_1_intern/task_5/MyExсeption.java
@@ -1,10 +1,10 @@
 package student_eduards_puzirevskis.lesson_12_exeptions.level_1_intern.task_5;
 
- class MyExeption extends Exception {
+ class MyExсeption extends Exception {
 
   private final int exceptionNumber;
 
-  MyExeption(int exceptionNumber,String message) {
+  MyExсeption(int exceptionNumber, String message) {
    super(message);
    this.exceptionNumber = exceptionNumber;
   }
