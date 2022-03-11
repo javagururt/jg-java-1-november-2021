570c4398af5201eff9b681415ac4c5f5d1cd0667
eduards1911 <76965658+eduards1911@users.noreply.github.com>
2022-03-11 20:36:57 +0200

    HW_Eduards_Puzirevskis

---
+++ src/main/java/student_eduards_puzirevskis/lesson_12_exeptions/level_1_intern/task_4/MyException.java
@@ -0,0 +1,9 @@
+package student_eduards_puzirevskis.lesson_12_exeptions.level_1_intern.task_4;
+
+ class MyException extends Exception {
+
+     public MyException(String message) {
+         super(message);
+     }
+
+}
