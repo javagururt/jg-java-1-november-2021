48998183ecb3a3d3a2ff36e5b777815b323033fc
eduards1911 <76965658+eduards1911@users.noreply.github.com>
2022-03-12 16:43:24 +0200

    HW_Eduards_Puzirevskis

---
+++ src/main/java/student_eduards_puzirevskis/lesson_12_exeptions/level_4_junior/OutOfMemoryErrorDemo.java
@@ -0,0 +1,26 @@
+package student_eduards_puzirevskis.lesson_12_exeptions.level_4_junior;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+class OutOfMemoryErrorDemo {
+
+//  public static void main(String[] args) {
+//
+//   List<String> stringList = new ArrayList<>();
+//   for (int i = 0; true; i++) {
+//    stringList.add(i,new String("Hello"));
+//   }
+/*
+Exception in thread "main" java.lang.OutOfMemoryError: Java heap space
+	at java.base/java.util.Arrays.copyOf(Arrays.java:3511)
+	at java.base/java.util.Arrays.copyOf(Arrays.java:3480)
+	at java.base/java.util.ArrayList.grow(ArrayList.java:237)
+	at java.base/java.util.ArrayList.grow(ArrayList.java:244)
+	at java.base/java.util.ArrayList.add(ArrayList.java:486)
+	at student_eduards_puzirevskis.lesson_12_exeptions.level_4_junior.OutOfMemoryErrorDemo.main(OutOfMemoryErrorDemo.java:13)
+ */
+
+//  }
+}
