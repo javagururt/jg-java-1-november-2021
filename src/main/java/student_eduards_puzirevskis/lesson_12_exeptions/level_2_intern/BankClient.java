a28b03949217b9b379aea7ac2e0c29cd7f9b7ca1
eduards1911 <76965658+eduards1911@users.noreply.github.com>
2022-03-12 13:45:33 +0200

    HW_Eduards_Puzirevskis

---
+++ src/main/java/student_eduards_puzirevskis/lesson_12_exeptions/level_2_intern/BankClient.java
@@ -0,0 +1,22 @@
+package student_eduards_puzirevskis.lesson_12_exeptions.level_2_intern;
+
+ class BankClient {
+
+     private String uid;
+     private String fullName;
+
+  public BankClient(String uid, String fullName) {
+   this.uid = uid;
+   this.fullName = fullName;
+  }
+
+  public String getUid() {
+   return this.uid;
+  }
+
+  public String getFullName() {
+   return this.fullName;
+  }
+
+
+ }
