07fbb2d77e3676377259aac8d8d62cf49495b789
mihailsnikolajevs <mike2309@inbox.lv>
2022-03-13 15:44:13 +0200

    lesson9_level4_junior_task_10_productDatabaseOptional

---
+++ src/main/java/student_mihails_nikolajevs/lesson_9/level_4_junior_/TemperatureConverter.java
@@ -0,0 +1,7 @@
+package student_mihails_nikolajevs.lesson_9.level_4_junior_;
+
+public interface TemperatureConverter {
+
+    double convert(double celsiusTemperature);
+
+}
---
+++ src/main/java/student_mihails_nikolajevs/lesson_9/level_4_junior_/WeatherTemperatureCelsiusConverter.java
@@ -0,0 +1,16 @@
+package student_mihails_nikolajevs.lesson_9.level_4_junior_;
+/*
+Сайт прогноза погоды на данный момент показывает
+температуру в градусах по цельсию.
+Для выхода на международный рынок сайту нужно
+уметь конвертировать температуру в кельвины и форенгейты.
+Попробуйте самостоятельно спроектировать (придумать)
+и реализовать решение для данной задачи.
+ */
+class WeatherTemperatureCelsiusConverter implements TemperatureConverter {
+
+      @Override
+      public double convert(double celsiusTemperature) {
+          return celsiusTemperature;
+      }
+}
