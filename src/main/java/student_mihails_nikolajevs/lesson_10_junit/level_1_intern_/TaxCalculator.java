02246654b0625d8072f2eb61fc943468bb5de18e
mihailsnikolajevs <mike2309@inbox.lv>
2022-03-13 17:58:43 +0200

    lesson10_level1_intern_taxCalculator

--- src/main/java/student_mihails_nikolajevs/lesson_10_junit/level_1_intern_/TaxCalculator.java
+++ src/main/java/student_mihails_nikolajevs/lesson_10_junit/level_1_intern_/TaxCalculator.java
@@ -3,7 +3,5 @@ package student_mihails_nikolajevs.lesson_10_junit.level_1_intern_;
 import java.math.BigDecimal;
 
 interface TaxCalculator {
-
      BigDecimal calculateTax(BigDecimal income);
-
 }
