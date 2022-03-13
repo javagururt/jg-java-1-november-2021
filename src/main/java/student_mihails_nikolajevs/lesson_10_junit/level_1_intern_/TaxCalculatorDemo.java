9bb650cdeb035186ba08809c4afeeb814de1fd84
mihailsnikolajevs <mike2309@inbox.lv>
2022-03-13 17:46:59 +0200

    lesson9_level4_junior_task_10_productDatabaseOptional

---
+++ src/main/java/student_mihails_nikolajevs/lesson_10_junit/level_1_intern_/TaxCalculatorDemo.java
@@ -0,0 +1,15 @@
+package student_mihails_nikolajevs.lesson_10_junit.level_1_intern_;
+
+import java.math.BigDecimal;
+
+public class TaxCalculatorDemo {
+
+    public static void main(String[] args) {
+
+        TaxCalculatorImpl taxCalculator = new TaxCalculatorImpl();
+
+        System.out.println(taxCalculator.calculateTax(BigDecimal.valueOf(35000)));
+        System.out.println(taxCalculator.calculateTax(BigDecimal.valueOf(50000)));
+
+    }
+}
---
+++ src/main/java/student_mihails_nikolajevs/lesson_10_junit/level_1_intern_/TaxCalculatorImpl.java
@@ -0,0 +1,31 @@
+package student_mihails_nikolajevs.lesson_10_junit.level_1_intern_;
+
+import java.math.BigDecimal;
+/*
+Вам поручили разработать калькулятор налогов.
+В стране используется прогрессивная ставка налога.
+
+Правило номер один:
+- доход до 20000 облагается 25% налогом.
+
+Создайте интерфейс TaxCalculator:
+
+interface TaxCalculator {
+
+    BigDecimal calculateTax(BigDecimal income);
+
+}
+ */
+class TaxCalculatorImpl implements TaxCalculator {
+
+    @Override
+    public BigDecimal calculateTax(BigDecimal income) {
+        int compareResult = income.compareTo(BigDecimal.valueOf(20000));
+        if (compareResult > 0) {
+            return (income.subtract(BigDecimal.valueOf(20000))
+            .multiply(BigDecimal.valueOf(0.40))
+                    .add(BigDecimal.valueOf(5000)));
+        }
+        return income.multiply(BigDecimal.valueOf(0.25));
+    }
+}
