0a371335b024091cc1bae7ebaac23db03ee480b2
eduards1911 <76965658+eduards1911@users.noreply.github.com>
2022-03-15 00:22:03 +0200

    HW_Eduards_Puzirevskis

---
+++ src/main/java/student_eduards_puzirevskis/lesson_14/level_3_4_junior/Trader.java
@@ -0,0 +1,25 @@
+package student_eduards_puzirevskis.lesson_14.level_3_4_junior;
+
+ class Trader {
+
+     private String name;
+     private String city;
+
+     public Trader(String name, String city) {
+         this.name = name;
+         this.city = city;
+     }
+
+     public String getName() {
+         return this.name;
+     }
+
+     public String getCity() {
+         return this.city;
+     }
+
+     public String toString() {
+         return "Trader:"+this.name + " in " + this.city;
+     }
+
+}
