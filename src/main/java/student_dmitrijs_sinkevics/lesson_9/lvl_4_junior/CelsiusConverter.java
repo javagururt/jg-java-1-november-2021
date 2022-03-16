6e90518abdbe4a525d038643542c7884cec42b10
dimon <dmitrij.sinkevics@gmail.com>
2022-03-16 12:29:58 +0200

    //

---
+++ src/main/java/student_dmitrijs_sinkevics/lesson_9/lvl_4_junior/CelsiusConverter.java
@@ -0,0 +1,8 @@
+package student_dmitrijs_sinkevics.lesson_9.lvl_4_junior;
+
+public class CelsiusConverter implements TemperatureConverter {
+    @Override
+    public double convert(double celsiusTemperature) {
+        return celsiusTemperature;
+    }
+}
