32b7f375c9c97d71b05f377901bc6affa64d733d
ValeriaKul <lera.dribnyuk@mail.ru>
2022-03-20 21:48:20 +0200

    lesson 12 level 3

---
+++ src/main/java/student_valeriia_kulikova/lesson_12_exceptions/homework/level_3_junior/NullPointerExceptionDemo.java
@@ -0,0 +1,11 @@
+package student_valeriia_kulikova.lesson_12_exceptions.homework.level_3_junior;
+
+public class NullPointerExceptionDemo {
+    public static void main(String[] args) {
+        String text = null;
+        System.out.println(text.length());
+    }
+}
+/*  Exception in thread "main" java.lang.NullPointerException:
+    Cannot invoke "String.length()" because "text" is null
+	at NullPointerExceptionDemo.main(NullPointerExceptionDemo.java:6) */
---
+++ src/main/java/student_valeriia_kulikova/lesson_12_exceptions/homework/level_3_junior/WrongUserInputHandlingDemo.java
@@ -0,0 +1,26 @@
+package student_valeriia_kulikova.lesson_12_exceptions.homework.level_3_junior;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class WrongUserInputHandlingDemo {
+    public static void main(String[] args) throws IOException {
+        UI();
+
+    }
+    public static void UI () throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        boolean check = false;
+        while (!check) {
+            try {
+                System.out.println("Enter integer number: ");
+                int number = scanner.nextInt();
+                System.out.println("Thank you! Your number is: " + number);
+                check = true;
+            } catch (Exception e) {
+                scanner.next();
+                System.out.println("Wrong type!");
+            }
+        }
+    }
+}
