252f51770f2bc43f6a7295159a3c16a1a1dd7c55
mihailsnikolajevs <mike2309@inbox.lv>
2022-03-23 11:32:33 +0200

    lesson12_level1_intern_task_5_ExceptionDemo_

---
+++ src/main/java/student_mihails_nikolajevs/lesson_12_exceptions_/level_1_intern_/task_5/Exception.java
@@ -0,0 +1,10 @@
+package student_mihails_nikolajevs.lesson_12_exceptions_.level_1_intern_.task_5;
+
+public class Exception {
+
+    protected String getMessage() {
+
+        return null;
+    }
+
+}
