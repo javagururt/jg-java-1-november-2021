30732b90b0b3111abd9daa1cbd65002a24e61659
mihailsnikolajevs <mike2309@inbox.lv>
2022-03-25 22:08:51 +0200

    lesson12_level1_intern_task_5_exceptionDemo

--- src/main/java/student_mihails_nikolajevs/lesson_12_exceptions_/level_1_intern_/task_5/ExceptionDemo.java
+++ src/main/java/student_mihails_nikolajevs/lesson_12_exceptions_/level_1_intern_/task_5/ExceptionDemo.java
@@ -7,32 +7,48 @@ package student_mihails_nikolajevs.lesson_12_exceptions_.level_1_intern_.task_5;
 разные способы создания экземпляра класса Exception.
  */
 
-import java.util.Scanner;
-
 public class ExceptionDemo extends Exception {
 
+    public ExceptionDemo() {
+    }
+
+    public ExceptionDemo(String message) {
+        super(message);
+    }
+
+    public ExceptionDemo(String message, Throwable cause) {
+        super(message);
+    }
+
     public static void main(String[] args) {
-        ExceptionDemo mc = new ExceptionDemo();
-        String str = mc.getMessage();
+
+        ExceptionDemo mc1 = new ExceptionDemo();
+        String str = mc1.getMessage();
+        ExceptionDemo mc2 = new ExceptionDemo("Checked Exception!");
+        ExceptionDemo mc3 = new ExceptionDemo("Checked Exception!", null);
         System.out.println("str = " + str);
     }
+}
+  //  class TriangleException extends Exception {
+    //    public TriangleException(String message) {
+  //          super(message);
+     //   }
 
-    class TriangleException extends Exception {
-        public String toString(){
-           return "Error. Bad sides of Triangle." ;
-        }
-    }
+    //    public String toString(){
+    //       return "Error. Bad sides of Triangle." ;
+    //    }
+   // }
     // обьявить класс треугольник
-    class Triangle{
+  //  class Triangle{
 
         // стороны треугольника
-        private double a,b,c;
+      //  private double a,b,c;
 
         // конструктор по умолчанию
-        public Triangle(){
-        }
+   //     public Triangle(){
+
     // равносторонний треугольник с длиной стороны 1
     // public double  a = b = c = 1;
-    }
+ //   }
 
-}
+//}
