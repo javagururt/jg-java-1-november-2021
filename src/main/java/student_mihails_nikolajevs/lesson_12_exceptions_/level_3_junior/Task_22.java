88decb87aaa79248825400a69a2ab30b63b07219
mihailsnikolajevs <mike2309@inbox.lv>
2022-03-26 01:48:29 +0200

    lesson12_level3_junior_task_22_throwableClass_

---
+++ src/main/java/student_mihails_nikolajevs/lesson_12_exceptions_/level_3_junior/Task_22.java
@@ -0,0 +1,18 @@
+package student_mihails_nikolajevs.lesson_12_exceptions_.level_3_junior;
+/*
+Какой класс в Java является главным в иерархии классов ошибок
+
+1. Raise
+
+2. Exception
+
+3. Error
+
+4. Throwable
+ */
+public class Task_22 {
+
+// The Base Class for All Exceptions is the Throwable Class.
+// Two Classes are Inherited from It: Error and Exception.
+
+}
