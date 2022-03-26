44c3ace11b20dc118321ba834a093e47ed26e142
mihailsnikolajevs <mike2309@inbox.lv>
2022-03-26 14:12:04 +0200

    lesson12_level_4_junior_task_26_stackOverFlowExample_

---
+++ src/main/java/student_mihails_nikolajevs/lesson_12_exceptions_/level_4_junior_/StackOverFlowExample.java
@@ -0,0 +1,19 @@
+package student_mihails_nikolajevs.lesson_12_exceptions_.level_4_junior_;
+/*
+Напишите программу, которая вылетает с StackOverflowError.
+Подсказка: Вызовите метод в рекурсии (сам себя).
+Память JVM не безгранична!
+ */
+
+public class StackOverFlowExample {
+
+    public static void main(String[] args) {
+        System.out.println(add5(1));
+    }
+    public static int add5(int a) {
+        return add5(a) + 5;             // add5(a) будет называть себя, а затем снова называть себя и т. д.
+    }
+}
+
+// StackOverflowError в основном - это когда вы пытаетесь сделать что-то, что, скорее всего,
+// называет себя , и продолжается бесконечно (или пока оно не даст StackOverflowError).
