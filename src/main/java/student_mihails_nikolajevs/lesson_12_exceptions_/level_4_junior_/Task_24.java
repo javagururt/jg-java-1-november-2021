8678ea4229e696f4ee425160efc3b2eb7617a705
mihailsnikolajevs <mike2309@inbox.lv>
2022-03-26 13:22:49 +0200

    lesson12_level4_junior_task_24_exceptions_

---
+++ src/main/java/student_mihails_nikolajevs/lesson_12_exceptions_/level_4_junior_/Task_24.java
@@ -0,0 +1,40 @@
+package student_mihails_nikolajevs.lesson_12_exceptions_.level_4_junior_;
+/*
+Знакомство с классами ошибок.
+
+Найдите в документации Java (javadoc) для чего испольхуются
+следующие ошибки:
+- ArrayIndexOutOfBoundsException
+- IllegalArgumentException
+- IOException
+- StackOverflowError
+- OutOfMemoryError
+- NumberFormatException
+ */
+public class Task_24 {
+
+// 1. ArrayIndexOutOfBoundsException - это исключение, появляющееся во время выполнения. Оно возникает тогда,
+//    когда мы пытаемся обратиться к элементу массива по отрицательному или превышающему размер массива индексу.
+
+// 2. IllegalArgumentException - Исключение случается , когда JVM пытается передать методу неподходящий аргумент
+//    или аргумент неправильного типа.
+
+// 3. IOException - Сигналы, что исключение ввода-вывода некоторого вида произошло.
+//    Этот класс является общим классом исключений, произведенных отказавшими или прерванными операциями
+//    ввода-вывода.
+
+// 4. StackOverflowError - Ошибка сигнализирует о том, что памяти больше нет. Она расширяет класс VirtualMachineError,
+//    что указывает на то, что JVM (виртуальная машина Java) повреждена или у нее закончились ресурсы и
+//    она не может работать.
+
+//  5. OutOfMemoryError - The “OutOfMemoryError Java Heap Space” error states that the server ran out of memory (RAM)
+//     while starting up or while it was running. The nature of this error is very simple,
+//     you do not have enough RAM to run the server or you ran out of RAM while the server was loading
+//     in its processes.
+
+//  6. NumberFormatException - Это неконтролируемое исключение, выводится, когда он не может преобразовать
+//     строку (String) в тип числа. Так как это не контролируется, Java не заставит нас обрабатывать или объявить его.
+
+
+
+}
