7faf2212e88f064f8445cc4bf8ca29d909d25fd8
mihailsnikolajevs <mike2309@inbox.lv>
2022-03-29 13:08:29 +0300

    lesson10_junit_level_3_junior_gameOfLifeNextGenerationDemo_

--- src/main/java/student_mihails_nikolajevs/lesson_10_junit/level_3_junior/GameOfLifeNextGenerationCalculator.java
+++ src/main/java/student_mihails_nikolajevs/lesson_10_junit/level_3_junior/GameOfLifeNextGenerationCalculator.java
@@ -2,7 +2,73 @@ package student_mihails_nikolajevs.lesson_10_junit.level_3_junior;
 
 public class GameOfLifeNextGenerationCalculator {
 
+   public int [] check (String[][] lifeMap, int[] point) {
 
-    
+       int counter = 0;
+       for (int i = 0; i < lifeMap.length; i++) {
+           for (int j = 0; j < lifeMap.length; j++) {
+               int life = 0;
+               if (i + 1 < 4 && lifeMap[i +1][j].equals("@")) {
+                   life++;
+               }
+               if (i + 1 < 4 && j + 1 < 4 && lifeMap[i + 1][j + 1].equals("@")) {
+                   life++;
+               }
+               if (j + 1 < 4 && j - 1 >= 0 && lifeMap[i + 1][j - 1].equals("@")) {
+                   life++;
+               }
+               if (j + 1 < 4 && lifeMap[i][j + 1].equals("@")) {
+                   life++;
+               }
+               if (j - 1 >= 0 && lifeMap[i][j - 1].equals("@")) {
+                   life++;
+               }
+               if (i - 1 >= 0 && lifeMap[i - 1][j].equals("@")) {
+                   life++;
+               }
+               if (i - 1 >= 0 && j + 1 < 4 && lifeMap[i - 1][j + 1].equals("@")) {
+                   life++;
+               }
+               if (i - 1 >=0 && j - 1 >= 0 && lifeMap[i - 1][j - 1].equals("@")) {
+                   life++;
+               }
+               if (lifeMap[i][j].equals("@")) {
+                   if (life == 1)
+                       point[counter] = 1;
+                   else if (life == 2 || life == 3)
+                       point[counter] = 2;
+                   else if (life >= 4)
+                       point[counter] = 1;
+               } else {
+                   if (life == 3)
+                       point[counter] = 3;
+               }
+               counter++;
+           }
+       }
+       return point;
+   }
+   public String[][] calculate(String[][] lifeMap, int[] point) {
+       int counter = 0;
+       for (int i = 0; i < lifeMap.length; i++){
+           for (int j = 0; j < lifeMap.length; j++) {
+               if (point[counter] == 1)
+                   lifeMap[i][j] = "o";
+               if (point[counter] == 3) {lifeMap[i][j] = "@";
+                   counter++;
+               }
+          }
+      }return lifeMap;
+   }
 
+   public void printLifeMap(String[][] lifeMap) {
+       for (String[] strings : lifeMap) {
+           for (int j = 0; j < lifeMap.length; j++) {
+               if (j == lifeMap.length - 1)
+                   System.out.println(strings[j] + " ");
+               else
+                   System.out.print(strings[j] + " ");
+           }
+       }
+   }
 }
---
+++ src/main/java/student_mihails_nikolajevs/lesson_10_junit/level_3_junior/GameOfLifeNextGenerationDemo.java
@@ -0,0 +1,58 @@
+package student_mihails_nikolajevs.lesson_10_junit.level_3_junior;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class GameOfLifeNextGenerationDemo {
+
+    public static void main(String[] args) {
+
+        String[][] lifeMap = getFieldSizeFromUser();
+
+        GameOfLifeNextGenerationCalculator Demo = new GameOfLifeNextGenerationCalculator();
+
+        getRandomField(lifeMap);
+
+        while (true) {
+
+            System.out.println("Original state:");
+            Demo.printLifeMap(lifeMap);
+            System.out.println("----------------");
+
+            System.out.println("Enter \"0\" To Get Next Generation");
+            System.out.println("Enter Any Integer To EXIT!");
+            int exitBottom = new Scanner(System.in).nextInt();
+            if (exitBottom != 0) {
+                break;
+            }
+            int[] point = Demo.check(lifeMap, new int[lifeMap.length * lifeMap.length]);
+            lifeMap = Demo.calculate(lifeMap, point).clone();
+            Demo.printLifeMap(lifeMap);
+            System.out.println("-----------------");
+        }
+    }
+
+    private static String[][] getFieldSizeFromUser() {
+        System.out.println(" HELLO!!! Let's Play The GAME OF LIFE!!! ");
+        System.out.println(" Please Enter Your Size Of Field: ");
+        Scanner in = new Scanner(System.in);
+        if (in.hasNextInt()) {
+            int fieldLength = in.nextInt();
+            return new String[fieldLength][fieldLength];
+        } else {
+            throw new IllegalArgumentException("Sorry ! Its Not INTEGER!!!");
+        }
+    }
+
+    private static void getRandomField(String[][] lifeMap) {
+        for (int i = 0; i < lifeMap.length; i++) {
+            for (int j = 0; j < lifeMap.length; j++) {
+
+                int num = new Random().nextInt(2);
+                if (num == 1)
+                    lifeMap[i][j] = "@";
+                else lifeMap[i][j] = "o";
+            }
+        }
+    }
+}
