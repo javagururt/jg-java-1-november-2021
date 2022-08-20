2ab82a485484f78a0144c26c525a9a1dcb32bb9a
mihailsnikolajevs <mike2309@inbox.lv>
2022-08-21 00:45:53 +0300

    Lesson3_level1_intern_VarSecondExample

---
+++ src/main/java/student_mihails_nikolajevs/lesson_2_var/VarSecondExample.java
@@ -0,0 +1,16 @@
+package student_mihails_nikolajevs.lesson_2_var;
+
+class VarSecondExample {
+    public static void main(String[] args) {
+
+        double x = 40;
+        double y = 20;
+        double result = x % y;
+
+        System.out.println("result = " + result);
+
+
+    }
+}
+
+
