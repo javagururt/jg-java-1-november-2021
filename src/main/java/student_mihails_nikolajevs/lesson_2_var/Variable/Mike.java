e12e0c14c04ffece6c45e2ace251a990285a51fd
mihailsnikolajevs <mike2309@inbox.lv>
2022-08-21 00:35:12 +0300

    Lesson3_level1_intern_Variable

---
+++ src/main/java/student_mihails_nikolajevs/lesson_2_var/Variable/Mike.java
@@ -0,0 +1,14 @@
+package student_mihails_nikolajevs.lesson_2_var.Variable;
+
+class Mike {
+    public static void main(String[] args) {
+
+        double x = 40;
+        double y = 20;
+        double result = x % y;
+
+        System.out.println("result = " + result);
+
+
+    }
+}
