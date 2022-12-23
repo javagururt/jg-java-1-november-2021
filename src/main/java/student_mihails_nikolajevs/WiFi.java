0f43328a6522b0b1028f5c2b5d8948da7f6db66f
mihailsnikolajevs <mike2309@inbox.lv>
2022-12-23 23:18:56 +0200

    HeiWiFi

---
+++ src/main/java/student_mihails_nikolajevs/WiFi.java
@@ -0,0 +1,8 @@
+package student_mihails_nikolajevs;
+
+public class WiFi {
+    public static void main(String[] args) {
+        System.out.println("Hei My Friend!!! Do U Using WiFi???");
+    }
+
+}
