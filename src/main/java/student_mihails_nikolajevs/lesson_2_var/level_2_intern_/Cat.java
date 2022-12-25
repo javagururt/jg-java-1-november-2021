7006506230d0e0c23ceff8471cdb668f8e9b8801
mihailsnikolajevs <mike2309@inbox.lv>
2022-12-25 21:53:34 +0200

    Cat

---
+++ src/main/java/student_mihails_nikolajevs/lesson_2_var/level_2_intern_/Cat.java
@@ -0,0 +1,8 @@
+package student_mihails_nikolajevs.lesson_2_var.level_2_intern_;
+
+public class Cat {
+   String name;
+   int age;
+   boolean hungry;
+
+}
---
+++ src/main/java/student_mihails_nikolajevs/lesson_2_var/level_2_intern_/CatDemo.java
@@ -0,0 +1,25 @@
+package student_mihails_nikolajevs.lesson_2_var.level_2_intern_;
+
+public class CatDemo {
+    public static void main(String[] args) {
+        Cat myCat = new Cat();
+        String catName = myCat.name;
+        int catAge = myCat.age;
+        boolean catHungry = myCat.hungry;
+
+        System.out.println(catName);
+        System.out.println(catAge);
+        System.out.println(catHungry);
+
+        myCat.name = "Vasja";
+        System.out.println(myCat.name);
+        Cat secondCat = new Cat();
+        secondCat.name = "Barsik";
+        System.out.println(secondCat.name);
+        Cat thirdCat = new Cat();
+        thirdCat.name = "Bella";
+        System.out.println(thirdCat.name);
+
+    }
+
+}
