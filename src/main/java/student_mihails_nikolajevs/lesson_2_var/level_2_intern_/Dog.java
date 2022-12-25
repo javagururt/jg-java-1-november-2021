ebef04a4f519a020872c3ecda2e010b66463abc2
mihailsnikolajevs <mike2309@inbox.lv>
2022-12-25 22:27:28 +0200

    Cat

---
+++ src/main/java/student_mihails_nikolajevs/lesson_2_var/level_2_intern_/Dog.java
@@ -0,0 +1,10 @@
+package student_mihails_nikolajevs.lesson_2_var.level_2_intern_;
+
+public class Dog {
+    String name;
+
+    public Dog(String name) {
+        this.name = name;
+    }
+
+}
---
+++ src/main/java/student_mihails_nikolajevs/lesson_2_var/level_2_intern_/DogDemo.java
@@ -0,0 +1,14 @@
+package student_mihails_nikolajevs.lesson_2_var.level_2_intern_;
+
+import java.util.Scanner;
+
+ public class DogDemo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String dogName = scanner.nextLine();
+
+        Dog myDog = new Dog(dogName);
+
+    }
+
+}
