1f6e70a24db539eded721a88cb72221deaae59bd
mihailsnikolajevs <mike2309@inbox.lv>
2022-12-25 14:13:48 +0200

    DoubleRes

---
+++ src/main/java/student_mihails_nikolajevs/lesson_14/wholeNumber.java
@@ -0,0 +1,17 @@
+package student_mihails_nikolajevs.lesson_14;
+
+
+
+public class wholeNumber {
+    public static void main(String[] args) {
+
+        double a = 10;
+        double b = 4;
+        double result = a / b;
+
+        System.out.println(result);
+    }
+}
+
+
+
