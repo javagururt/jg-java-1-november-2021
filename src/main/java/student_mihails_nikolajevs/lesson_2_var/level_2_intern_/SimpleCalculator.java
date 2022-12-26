1a81aaff1aa1596b4a1c4bf880c2e654da6b42b7
mihailsnikolajevs <mike2309@inbox.lv>
2022-12-26 22:15:15 +0200

    CalculatorDemo

---
+++ src/main/java/student_mihails_nikolajevs/lesson_2_var/level_2_intern_/SimpleCalculator.java
@@ -0,0 +1,10 @@
+package student_mihails_nikolajevs.lesson_2_var.level_2_intern_;
+
+public class SimpleCalculator {
+
+ public int sum(int a, int b){
+    int result = a + b;
+    return result;
+
+   }
+}
---
+++ src/main/java/student_mihails_nikolajevs/lesson_2_var/level_2_intern_/SimpleCalculatorDemo.java
@@ -0,0 +1,21 @@
+package student_mihails_nikolajevs.lesson_2_var.level_2_intern_;
+
+import java.util.Scanner;
+
+public class SimpleCalculatorDemo {
+    public static void main(String[] args) {
+        SimpleCalculator calculator = new SimpleCalculator();
+
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please Enter First Number: ");
+        int a = scanner.nextInt();
+        System.out.println("Please Enter Second Number: ");
+        int b = scanner.nextInt();
+
+        int userSumResult = calculator.sum(a,b);
+        System.out.println("Sum Result : " + userSumResult);
+
+    }
+
+}
