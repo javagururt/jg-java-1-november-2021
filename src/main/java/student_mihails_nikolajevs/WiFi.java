c6d5e4887ec7b15bc6fc96ba4a9f11199450fdbd
mihailsnikolajevs <mike2309@inbox.lv>
2024-07-22 19:04:42 +0300

    SwitchExampleMik

--- src/main/java/student_mihails_nikolajevs/WiFi.java
+++ src/main/java/student_mihails_nikolajevs/WiFi.java
@@ -2,6 +2,7 @@ package student_mihails_nikolajevs;
 
 public class WiFi {
     public static void main(String[] args) {
+
         System.out.println("Hei My Friend!!! Do U Using WiFi???");
     }
 
