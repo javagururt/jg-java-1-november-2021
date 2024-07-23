ccf2afffbc7e228b44eec52ad77d13edd16bf91d
mihailsnikolajevs <mike2309@inbox.lv>
2024-07-24 00:58:33 +0300

    SecondHelloEverybody

--- src/main/java/student_mihails_nikolajevs/RandomNumberTest.java
+++ src/main/java/student_mihails_nikolajevs/RandomNumberTest.java
@@ -3,7 +3,9 @@ package student_mihails_nikolajevs;
 import java.util.Random;
 
 public class RandomNumberTest {
+
     public static void main(String[] args) {
+
         Random RandomGenerator = new Random();
         int randomNumber = RandomGenerator.nextInt(101);
         System.out.println(randomNumber);
