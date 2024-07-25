890eb21fc4f43dedb36672f7245803e02579b898
mihailsnikolajevs <mike2309@inbox.lv>
2024-07-25 14:47:50 +0300

    SetName

--- src/main/java/student_mihails_nikolajevs/SecondHelloEverybody.java
+++ src/main/java/student_mihails_nikolajevs/SecondHelloEverybody.java
@@ -2,6 +2,7 @@ package student_mihails_nikolajevs;
 
 public class SecondHelloEverybody {
     public static void main(String[] args) {
+
         System.out.println("Second Hello EveryBody!!!");
     }
 }
