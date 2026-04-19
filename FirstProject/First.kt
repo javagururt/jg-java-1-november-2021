60467243abb3ec274b8904c208ced006189af953
mihailsnikolajevs <mike2309@inbox.lv>
2026-04-19 15:45:42 +0300

    testHello

---
+++ FirstProject/First.kt
@@ -0,0 +1,7 @@
+class First {
+fun main() {
+    var a : Int = 5;
+    var name: String = "rectangle"
+
+}
+}
---
+++ src/main/java/student_mihails_nikolajevs/lesson_2_var/level_1_intern_/TestHello.java
@@ -0,0 +1,8 @@
+package student_mihails_nikolajevs.lesson_2_var.level_1_intern_;
+
+public class TestHello {
+
+    static void main() {
+        System.out.println("Hello World");
+    }
+}
